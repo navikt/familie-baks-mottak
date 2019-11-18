c6e185f16b86ee20c427d2592522acc8d97f3023
Helga Nyrud <helga.nyrud@nav.no>
2019-11-18 10:43:54 +0100

    Endret måten vi leser ut fra kafkakøen

--- src/main/kotlin/no/nav/familie/ba/mottak/hendelser/KafkaConsumer.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/hendelser/KafkaConsumer.kt
@@ -1,8 +1,9 @@
 package no.nav.familie.ba.mottak.hendelser
 
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,7 +12,7 @@ class KafkaConsumer {
     val log = LoggerFactory.getLogger(KafkaConsumer::class.java)
 
     @KafkaListener(topics = ["aapen-person-pdl-leesah-v1"])
-    fun listen(@Payload message : String) {
-        log.info("KafkaConsumer lytter på melding {} ", message)
+    fun listen(cr: ConsumerRecord<String, GenericRecord>) {
+        log.info("Melding mottatt på topic: {}, partisjon: {} med offset: {}, og verdi: {}", cr.topic(), cr.partition(), cr.offset(), cr.value())
     }
 }
