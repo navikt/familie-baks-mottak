665ce5199a5fc463a19d8be228afbd104a0eb78b
Martine Enger <marenger@student.matnat.uio.no>
2019-11-25 11:38:21 +0100

    Rydd i properties, legg til logfilter i appconfig

--- src/main/kotlin/no/nav/familie/ba/mottak/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/config/ApplicationConfig.kt
@@ -1,7 +1,27 @@
 package no.nav.familie.ba.mottak.config
 
+import no.nav.familie.log.filter.LogFilter
 import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.context.annotation.Bean
 
 @SpringBootConfiguration
 class ApplicationConfig {
+
+    @Bean
+    fun servletWebServerFactory(): ServletWebServerFactory {
+        val serverFactory = JettyServletWebServerFactory()
+        serverFactory.port = 8080
+        return serverFactory
+    }
+
+    @Bean
+    fun logFilter(): FilterRegistrationBean<LogFilter> {
+        val filterRegistration = FilterRegistrationBean<LogFilter>()
+        filterRegistration.filter = LogFilter()
+        filterRegistration.order = 1
+        return filterRegistration
+    }
 }
