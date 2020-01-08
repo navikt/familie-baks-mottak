6c97fe785df33c277b65e814aac061088cb8aa3e
Henning Solberg <henning.solberg@nav.no>
2020-01-08 20:38:07 +0100

    Lagt inn restarting av kafkaListenerContainer ved feil Lagt inn custom kafka error logger for å unngå logging av records ved feil.

---
+++ src/main/kotlin/no/nav/familie/ba/mottak/config/CustomKafkaLoggingErrorHandler.kt
@@ -0,0 +1,21 @@
+package no.nav.familie.ba.mottak.config
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import org.springframework.kafka.listener.ErrorHandler
+
+
+class CustomKafkaLoggingErrorHandler : ErrorHandler {
+    override fun handle(thrownException: Exception, record: ConsumerRecord<*, *>) {
+        LOGGER.error("Problemer med prosessering")
+        SECURE_LOGGER.error("Problemer med prosessering av $record", thrownException)
+    }
+
+    companion object {
+        val LOGGER: Logger = LoggerFactory.getLogger(CustomKafkaLoggingErrorHandler::class.java)
+        val SECURE_LOGGER: Logger = LoggerFactory.getLogger("secureLogger")
+    }
+}
---
+++ src/main/kotlin/no/nav/familie/ba/mottak/config/RestartingErrorHandler.kt
@@ -0,0 +1,31 @@
+package no.nav.familie.ba.mottak.config
+
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.listener.ContainerAwareErrorHandler
+import org.springframework.kafka.listener.ContainerStoppingErrorHandler
+import org.springframework.kafka.listener.MessageListenerContainer
+import org.springframework.scheduling.TaskScheduler
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+
+class RestartingErrorHandler(private val taskScheduler: TaskScheduler) : ContainerAwareErrorHandler {
+    override fun handle(thrownException: Exception,
+                        data: List<ConsumerRecord<*, *>?>?,
+                        consumer: Consumer<*, *>?,
+                        container: MessageListenerContainer) {
+        taskScheduler.schedule(Restarter(container), Instant.now().plus(1, ChronoUnit.MINUTES))
+        STOPPER.handle(thrownException, data, consumer, container)
+    }
+
+    private class Restarter(val container : MessageListenerContainer) : Runnable {
+        override fun run() {
+            container.start()
+        }
+    }
+
+    companion object {
+        private val STOPPER = ContainerStoppingErrorHandler()
+    }
+}
