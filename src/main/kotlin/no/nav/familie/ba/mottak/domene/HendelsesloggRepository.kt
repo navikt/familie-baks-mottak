36815d1c3691b71a326b17e5002ee0f2fc9450a7
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-01-08 11:56:55 +0100

    Fjerner unødvendig query-funksjon.

--- src/main/kotlin/no/nav/familie/ba/mottak/domene/HendelsesloggRepository.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/domene/HendelsesloggRepository.kt
@@ -3,13 +3,10 @@ package no.nav.familie.ba.mottak.domene
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
 import org.springframework.stereotype.Repository
-import java.util.*
 import javax.persistence.LockModeType
 
 @Repository
 interface HendelsesloggRepository : JpaRepository<Hendelseslogg, Long> {
-
-    override fun findById(id: Long): Optional<Hendelseslogg>
 
     @Lock(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
     fun save(hendelseslogg: Hendelseslogg): Hendelseslogg
