68d1ce50b62ce81cd2897a1e7200f300d7f1b29d
Henning Solberg <henning.solberg@nav.no>
2020-02-18 15:05:30 +0100

    Fjerner feilede tasks i preprod hver uke.

---
+++ src/main/kotlin/no/nav/familie/ba/mottak/task/FjernGamleFeiledeTasksIPreprod.kt
@@ -0,0 +1,28 @@
+package no.nav.familie.ba.mottak.task
+
+import no.nav.familie.prosessering.domene.Avvikstype
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+@Profile("!prod")
+class FjernGamleFeiledeTasksIPreprod(val taskRepository: TaskRepository){
+
+    @Scheduled(cron = "5 8 * * Sun")
+    fun fjernGamleFeiledeTasksIPreprod() {
+        LOG.info("Fjerner gamle feilede tasks")
+
+        for(task: Task in taskRepository.finnAlleFeiledeTasks()) {
+            task.avvikshåndter(avvikstype = Avvikstype.ANNET, årsak = "Rydder", endretAv = "VL")
+            taskRepository.save(task)
+        }
+    }
+
+    companion object {
+        val LOG = LoggerFactory.getLogger(FjernGamleFeiledeTasksIPreprod::class.java)
+    }
+}
