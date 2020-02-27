666a464bb34fd88cfff407ad33c9d7abd857f4ae
Henning Ivan Solberg <hensol@users.noreply.github.com>
2020-02-27 14:12:58 +0100

    Lagt inn toggle for fødselshendelser (#35)
    * Lagt inn featuretoggle for fødselshendelser

--- src/main/kotlin/no/nav/familie/ba/mottak/task/SendTilSakTask.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/task/SendTilSakTask.kt
@@ -1,19 +1,30 @@
 package no.nav.familie.ba.mottak.task
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import no.nav.familie.ba.mottak.config.FeatureToggleService
 import no.nav.familie.ba.mottak.domene.NyBehandling
 import no.nav.familie.ba.mottak.integrasjoner.SakService
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 @TaskStepBeskrivelse(taskStepType = SendTilSakTask.TASK_STEP_TYPE, beskrivelse = "Send til sak")
-class SendTilSakTask(private val sakService: SakService) : AsyncTaskStep {
+class SendTilSakTask(private val sakService: SakService, private val featureToggleService: FeatureToggleService) : AsyncTaskStep {
+
+    val logger: Logger = LoggerFactory.getLogger(SendTilSakTask::class.java)
+
 
     override fun doTask(task: Task) {
-        sakService.sendTilSak(jacksonObjectMapper().readValue(task.payload, NyBehandling::class.java))
+        if (featureToggleService.isEnabled("familie-ba-mottak.behandle-fodselshendelse")) {
+            sakService.sendTilSak(jacksonObjectMapper().readValue(task.payload, NyBehandling::class.java))
+        } else {
+            logger.info("Behandler ikke fødselshendelse, feature er skrudd av i Unleash")
+        }
+
     }
 
     override fun onCompletion(task: Task) {
