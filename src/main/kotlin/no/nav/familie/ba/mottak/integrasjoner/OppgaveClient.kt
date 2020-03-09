6a8fcd55a238659487db3ef6a8eb3cc7c3c63d32
Tor Nærland <tor.nerland@nav.no>
2020-03-09 12:38:47 +0100

    endringer basert på tilbakemeldinger

--- src/main/kotlin/no/nav/familie/ba/mottak/integrasjoner/OppgaveClient.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/integrasjoner/OppgaveClient.kt
@@ -14,8 +14,6 @@ import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
 import java.net.URI
 import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 private val logger = LoggerFactory.getLogger(OppgaveClient::class.java)
 
@@ -25,43 +23,35 @@ class OppgaveClient @Autowired constructor(@param:Value("\${FAMILIE_INTEGRASJONE
     : AbstractRestClient(restOperations, "integrasjon") {
 
     @Retryable(value = [RuntimeException::class], maxAttempts = 3, backoff = Backoff(delay = 5000))
-    fun opprettJournalføringsoppgave(journalpostId: String): OppgaveResponse {
+    fun opprettJournalføringsoppgave(journalpost: Journalpost): OppgaveResponse {
         logger.info("Oppretter journalføringsoppgave for papirsøknad")
-
         val uri = URI.create("$integrasjonUri/oppgave")
-        val request = OpprettOppgave(ident = OppgaveIdent(ident = "", type = IdentType.Aktør),
-                                     saksId = null,
-                                     journalpostId = journalpostId,
-                                     tema = Tema.BAR,
-                                     oppgavetype = Oppgavetype.Journalføring,
-                                     fristFerdigstillelse = LocalDate.now().plusDays(1),
-                                     beskrivelse = lagOppgaveTekst(journalpostId),
-                                     enhetsnummer = null,
-                                     behandlingstema = "ab0180") // ordinær barnetrygd
 
         return Result.runCatching {
+        val request = OpprettOppgave(ident = OppgaveIdent(ident = journalpost.bruker!!.id, type = IdentType.Aktør),
+                                     saksId = null,
+                                     journalpostId = journalpost.journalpostId,
+                                     tema = Tema.BAR,
+                                     oppgavetype = Oppgavetype.Journalføring,
+                                     fristFerdigstillelse = LocalDate.now().plusDays(2), //TODO få denne til å funke på helg og eventuellle andre helligdager
+                                     beskrivelse = "",
+                                     enhetsnummer = journalpost.journalforendeEnhet,
+                                     behandlingstema = "ab0180")
             postForEntity<Ressurs<OppgaveResponse>>(uri, request)
         }.fold(
-            onSuccess = {
-                when {
-                    it == null -> error("Finner ikke ressurs")
-                    it.data == null -> error("Ressurs mangler data")
-                    it.status != Ressurs.Status.SUKSESS -> error("Ressurs returnerer 2xx men har ressurs status failure")
-
-                    else -> it.data!!
-                }
-            },
-            onFailure = {
-                throw IntegrasjonException("Kall mot integrasjon feilet ved opprettelse av oppgave", it, uri, null)
+            onSuccess = { response -> assertGyldig(response) },
+            onFailure = { throw IntegrasjonException("Kall mot integrasjon feilet ved opprettelse av oppgave", it, uri, null)
             }
         )
     }
 
-    private fun lagOppgaveTekst(journalpostId: String): String {
-        //TODO Tekst skal oppdateres når man får et forslag
-        var oppgaveTekst =
-            "----- Opprettet av familie-ba-sak ${LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)} --- \n"
-        oppgaveTekst += "Ny, midlertidig journalføring av papirsøknad om barnetrygd med journalpostId $journalpostId \n"
-        return oppgaveTekst
+    private fun assertGyldig(ressurs: Ressurs<OppgaveResponse>?): OppgaveResponse {
+        return when {
+            ressurs == null -> error("Finner ikke ressurs")
+            ressurs.data == null -> error("Ressurs mangler data")
+            ressurs.status != Ressurs.Status.SUKSESS -> error("Ressurs returnerer 2xx men har ressurs status failure")
+
+            else -> ressurs.data!!
+        }
     }
 }
--- src/main/kotlin/no/nav/familie/ba/mottak/task/OpprettOppgaveForJournalføringTask.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/task/OpprettOppgaveForJournalføringTask.kt
@@ -1,17 +1,27 @@
 package no.nav.familie.ba.mottak.task
 
+import no.nav.familie.ba.mottak.integrasjoner.JournalpostService
+import no.nav.familie.ba.mottak.integrasjoner.Journalstatus
 import no.nav.familie.ba.mottak.integrasjoner.OppgaveClient
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
 import org.springframework.stereotype.Service
 
 @Service
 @TaskStepBeskrivelse(taskStepType = OpprettOppgaveForJournalføringTask.TASK_STEP_TYPE, beskrivelse = "Opprett journalføringsoppgave")
-class OpprettOppgaveForJournalføringTask(private val oppgaveClient: OppgaveClient) : AsyncTaskStep {
+class OpprettOppgaveForJournalføringTask(private val journalpostService: JournalpostService,
+                                         private val oppgaveClient: OppgaveClient,
+                                         private val taskRepository: TaskRepository) : AsyncTaskStep {
 
     override fun doTask(task: Task) {
-        oppgaveClient.opprettJournalføringsoppgave(task.payload)
+        val journalpost = journalpostService.hentJournalpost(task.payload)
+
+        if (journalpost.journalstatus.equals(Journalstatus.MOTTATT)) {
+            task.metadata["oppgaveId"] = "${oppgaveClient.opprettJournalføringsoppgave(journalpost).oppgaveId}"
+            taskRepository.saveAndFlush(task)
+        }
     }
 
     companion object {
