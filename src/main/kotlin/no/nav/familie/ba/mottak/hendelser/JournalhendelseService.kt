55c426b648d8f163d097af973ce3cc27bd078521
Tor Nærland <tor.nerland@nav.no>
2020-04-21 10:09:30 +0200

    Utvider dokumentmottak til å lytte etter SKAN_*

--- src/main/kotlin/no/nav/familie/ba/mottak/hendelser/JournalhendelseService.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/hendelser/JournalhendelseService.kt
@@ -39,12 +39,12 @@ class JournalhendelseService(val journalpostClient: JournalpostClient,
 
             when (journalpost.journalstatus) {
                 Journalstatus.MOTTATT -> {
-                    when (journalpost.kanal) {
-                        "SKAN_NETS" -> {
+                    when {
+                        "SKAN_" == journalpost.kanal?.substring(0, 5) -> {
                             behandleSkanningHendelser(journalpost)
                         }
 
-                        "NAV_NO" -> {
+                        "NAV_NO" == journalpost.kanal -> {
                             behandleNavnoHendelser(journalpost)
                         }
 
