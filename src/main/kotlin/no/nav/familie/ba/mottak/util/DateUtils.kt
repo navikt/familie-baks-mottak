2d17ca56a4c5f36ead075dd92c34c41868fa3bb0
Stig Strøm <stig.helge.strom@nav.no>
2020-06-12 10:25:31 +0200

    Endre triggertid for MottaFødselsehendelser (#95)
    * Skip helg for kjøring
    
    * Optimaliseringer for e2e

--- src/main/kotlin/no/nav/familie/ba/mottak/util/DateUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/util/DateUtils.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ba.mottak.util
 
+import org.springframework.core.env.Environment
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -32,4 +33,38 @@ fun fristFerdigstillelse(daysToAdd: Long = 0): LocalDate {
     }
 
     return date.toLocalDate()
+}
+
+/**
+ * Skipper helger hvis ikke e2e.
+ */
+fun nesteGyldigeTriggertidFødselshendelser(minutesToAdd: Long = 0, environment: Environment): LocalDateTime {
+    var date = LocalDateTime.now().plusMinutes(minutesToAdd)
+
+    if (environment.activeProfiles.contains("e2e")) {
+        return date
+    }
+
+    when {
+        date.dayOfWeek == DayOfWeek.SATURDAY -> date = date.plusDays(2)
+        date.dayOfWeek == DayOfWeek.SUNDAY -> date = date.plusDays(1)
+        date.dayOfWeek == DayOfWeek.FRIDAY && date.hour >= 16 -> date = date.plusHours(56)
+    }
+
+    when {
+        date.dayOfMonth == 1 && date.month == Month.JANUARY -> date = date.plusDays(1)
+        date.dayOfMonth == 1 && date.month == Month.MAY -> date = date.plusDays(1)
+        date.dayOfMonth == 17 && date.month == Month.MAY -> date = date.plusDays(1).withHour(10)
+        date.dayOfMonth == 25 && date.month == Month.DECEMBER -> date = date.plusDays(2).withHour(10)
+        date.dayOfMonth == 26 && date.month == Month.DECEMBER -> date = date.plusDays(1).withHour(10)
+    }
+
+    when {
+        date.dayOfWeek == DayOfWeek.SATURDAY -> date = date.plusDays(2)
+        date.dayOfWeek == DayOfWeek.SUNDAY -> date = date.plusDays(1)
+        date.dayOfWeek == DayOfWeek.FRIDAY && date.hour >= 16 -> date = date.plusHours(56)
+        date.dayOfWeek == DayOfWeek.MONDAY && date.hour <= 8 -> date = date.withHour(10)
+    }
+
+    return date
 }
--- src/test/kotlin/no/nav/familie/ba/mottak/util/DateUtilsTest.kt
+++ src/test/kotlin/no/nav/familie/ba/mottak/util/DateUtilsTest.kt
@@ -1,10 +1,13 @@
 package no.nav.familie.ba.mottak.util
 
 import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import io.mockk.mockkStatic
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.springframework.core.env.Environment
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -22,11 +25,33 @@ class DateUtilsTest {
             "2020-12-26T00:00:00, 2020-12-28", //første arbeidsdag er mandag 28
             "2020-01-01T00:00:00, 2020-01-02"
     )
-    fun `skal returnere neste arbeidsdag`(input: LocalDateTime, expected: LocalDate) {
+    fun `fristFerdigstillelese skal returnere neste arbeidsdag`(input: LocalDateTime, expected: LocalDate) {
         mockkStatic(LocalDateTime::class)
 
         every { LocalDateTime.now() } returns input
 
         assertThat(fristFerdigstillelse()).isEqualTo(expected)
     }
+
+
+    @ParameterizedTest
+    @CsvSource(
+            "2020-06-09T13:37:00, 2020-06-10T13:37", //Neste dag er en arbeidsdag
+            "2020-06-12T13:37:00, 2020-06-15T13:37", //Neste dag er en lørdag. Venter til mandag samme tid
+            "2020-06-13T13:37:00, 2020-06-15T13:37", //Neste dag er en søndag. Venter til mandag samme tid
+            "2020-06-13T06:37:00, 2020-06-15T10:37", //Ikke kjøre før kl 10 mandag
+            "2020-06-11T16:01:00, 2020-06-15T10:01", //Ikke kjøre fredag etter 16. Vent til mandag
+            "2021-05-14T00:00:00, 2021-05-18T10:00" //14 mai er fredag, 17 mai er mandag og fridag, skal returnere 18 mai
+    )
+    fun `skal returnere neste arbeidsdag `(input: LocalDateTime, expected: LocalDateTime) {
+        mockkStatic(LocalDateTime::class)
+        mockk<Environment>(relaxed = true)
+
+
+        every { LocalDateTime.now() } returns input
+
+        assertThat(nesteGyldigeTriggertidFødselshendelser(1440, mockk<Environment>(relaxed = true))).isEqualTo(expected)
+    }
+
+
 }
