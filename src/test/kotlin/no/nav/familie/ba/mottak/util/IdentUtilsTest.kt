330fabe2629bff7e63b194dea1a88e4fc2c16d3b
Stig Strøm <stig.helge.strom@nav.no>
2020-07-03 10:10:23 +0200

    Ignorerer fnr som er BOST (#105)
    * sjekk om ident er bost
    
    * sjekk om ident er bost

--- src/test/kotlin/no/nav/familie/ba/mottak/util/IdentUtilsTest.kt
+++ src/test/kotlin/no/nav/familie/ba/mottak/util/IdentUtilsTest.kt
@@ -3,15 +3,28 @@ package no.nav.familie.ba.mottak.util
 import no.nav.familie.ba.mottak.domene.personopplysning.PersonIdent
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles("dev")
+
 class IdentUtilsTest {
     @Test
     fun testErDnummer() {
         Assertions.assertFalse(erDnummer(PersonIdent(id = null)))
         Assertions.assertFalse(erDnummer(PersonIdent(id = "12345678910")))
         Assertions.assertTrue(erDnummer(PersonIdent(id = "42345678910")))
+    }
+
+    @Test
+    fun `er BOST-nummer`() {
+        Assertions.assertFalse(erBostNummer(PersonIdent(id = null)))
+        Assertions.assertFalse(erBostNummer(PersonIdent(id = "10102022222")))
+        Assertions.assertTrue(erBostNummer(PersonIdent(id = "10201022222")))
+    }
+
+    @Test
+    fun `er FDAT nummer`() {
+        Assertions.assertFalse(erFDatnummer(PersonIdent(id = null)))
+        Assertions.assertFalse(erFDatnummer(PersonIdent(id = "10102022222")))
+        Assertions.assertTrue(erFDatnummer(PersonIdent(id = "10201000000")))
     }
 
 }
