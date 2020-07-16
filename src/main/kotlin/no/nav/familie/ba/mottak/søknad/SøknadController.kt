4d2a45d53aac702ea1452e515fe23a09139b74d0
Henrik Jackson <henrik.m.jackson@gmail.com>
2020-07-16 12:25:47 +0200

    Endepunkt tar imot multipart_form_data

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
@@ -3,19 +3,16 @@ package no.nav.familie.ba.mottak.søknad
 import main.kotlin.no.nav.familie.ba.søknad.Søknad
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["/api"], produces = [APPLICATION_JSON_VALUE])
 @Unprotected
 class SøknadController {
     @PostMapping(value= ["/soknad"], consumes = [MULTIPART_FORM_DATA_VALUE])
-    fun taImotSøknad(@RequestBody(required = true) søknad: Søknad): ResponseEntity<String> {
+    fun taImotSøknad(@RequestPart("søknad") søknad: Søknad): ResponseEntity<String> {
         return ResponseEntity.ok("Søknad mottatt OK")
     }
 }
