addec38072ddb1be94e29fbcf647cce8549b5423
idaame <ida.enholm@me.com>
2020-07-16 14:39:09 +0200

    sette opp autentisering ved kall på /api/soknad

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.mottak.søknad
 
 import main.kotlin.no.nav.familie.ba.søknad.Søknad
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.ResponseEntity
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["/api"], produces = [APPLICATION_JSON_VALUE])
-@Unprotected
+@ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
 class SøknadController {
     @PostMapping(value= ["/soknad"], consumes = [MULTIPART_FORM_DATA_VALUE])
     fun taImotSøknad(@RequestPart("søknad") søknad: Søknad): ResponseEntity<String> {
