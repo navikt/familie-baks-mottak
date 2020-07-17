17d939ec2e0b55bb882b99be02e16d7de43b4da7
Ida Merete Enholm <ida.enholm@me.com>
2020-07-17 14:09:14 +0200

    Ba-søknad: endepunkt for å motta søknadsdata (#111)
    * Opprett endepunkt til søknad
    
    * lage søknadscontroller
    
    * Endepunkt tar imot multipart_form_data
    
    * sette opp autentisering ved kall på /api/soknad
    
    * returnere kvittering
    
    * bytt til norsk navn på variabel
    
    * endre returtype til ressurs med kvittering
    
    * oppdatere kontrakter versjon og fikse feil
    
    * kommentere ut kode for azuread
    
    * Oppdater kontrakt-versjon
    
    Co-authored-by: Henrik Jackson <henrik.m.jackson@gmail.com>
    Co-authored-by: jonaengs <32123499+jonaengs@users.noreply.github.com>

---
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
@@ -0,0 +1,21 @@
+package no.nav.familie.ba.mottak.søknad
+
+import main.kotlin.no.nav.familie.ba.søknad.Søknad
+import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.ResponseEntity
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+
+@RestController
+@RequestMapping(path = ["/api"], produces = [APPLICATION_JSON_VALUE])
+@ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
+class SøknadController {
+    @PostMapping(value= ["/soknad"], consumes = [MULTIPART_FORM_DATA_VALUE])
+    fun taImotSøknad(@RequestPart("søknad") søknad: Søknad): ResponseEntity<Ressurs<Kvittering>> {
+        return ResponseEntity.ok(Ressurs.success(Kvittering("Søknad er mottatt", LocalDateTime.now())))
+    }
+}
