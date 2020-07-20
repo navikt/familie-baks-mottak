4aac9612b75b8778a0528aefbf88e70e99c87900
idaame <ida.enholm@me.com>
2020-07-20 09:38:39 +0200

    logge om feature toggle er på eller av

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
@@ -4,6 +4,8 @@ import main.kotlin.no.nav.familie.ba.søknad.Søknad
 import no.nav.familie.ba.mottak.config.FeatureToggleService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
@@ -14,13 +16,17 @@ import java.time.LocalDateTime
 @RequestMapping(path = ["/api"], produces = [APPLICATION_JSON_VALUE])
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
 class SøknadController(val featureToggleService: FeatureToggleService) {
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
     @PostMapping(value = ["/soknad"], consumes = [MULTIPART_FORM_DATA_VALUE])
     fun taImotSøknad(@RequestPart("søknad") søknad: Søknad): ResponseEntity<Ressurs<Kvittering>> {
 
         return if (featureToggleService.isEnabled("familie-ba-mottak.lagre-soknad")) {
-            ResponseEntity.ok(Ressurs.success(Kvittering("Søknad er mottatt", LocalDateTime.now())))
+            log.info("Lagring av søknad er aktivert")
+            ResponseEntity.ok(Ressurs.success(Kvittering("Søknad er lagret", LocalDateTime.now())))
         } else {
-            ResponseEntity.ok(Ressurs.success(Kvittering("Søknad er mottatt", LocalDateTime.now())))
+            log.info("Lagring av søknad er deaktivert")
+            ResponseEntity.ok(Ressurs.success(Kvittering("Lagring av søknad er deaktivert", LocalDateTime.now())))
         }
     }
 }
