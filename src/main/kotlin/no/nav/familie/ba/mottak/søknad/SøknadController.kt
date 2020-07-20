da12600926294ae049c7a3e517c5bc6c11cb5c60
jonatan <jonaengs@gmail.com>
2020-07-20 09:50:11 +0200

    ta i bruk søknadservice i søknadcontroller

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
@@ -4,6 +4,7 @@ import main.kotlin.no.nav.familie.ba.søknad.Søknad
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.api.Unprotected
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
@@ -13,9 +14,11 @@ import java.time.LocalDateTime
 @RestController
 @RequestMapping(path = ["/api"], produces = [APPLICATION_JSON_VALUE])
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
-class SøknadController {
+class SøknadController(private val søknadService: SøknadService) {
+
     @PostMapping(value= ["/soknad"], consumes = [MULTIPART_FORM_DATA_VALUE])
     fun taImotSøknad(@RequestPart("søknad") søknad: Søknad): ResponseEntity<Ressurs<Kvittering>> {
+        søknadService.motta(søknad)
         return ResponseEntity.ok(Ressurs.success(Kvittering("Søknad er mottatt", LocalDateTime.now())))
     }
 }
