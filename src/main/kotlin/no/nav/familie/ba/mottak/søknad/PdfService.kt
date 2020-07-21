8c2bc50e173f142ea31056ee1d635a654bf08386
eirikdahlen <eirik.dahlen@nav.no>
2020-07-21 18:42:21 +0200

    fjern logging

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/PdfService.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/PdfService.kt
@@ -1,6 +1,5 @@
 package no.nav.familie.ba.mottak.søknad
 
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,6 +8,5 @@ class PdfService(private val søknadRepository: SøknadRepository) {
     fun lagPdf(id: String) {
         val dbSøknad = søknadRepository.hentDBSøknad(id.toLong()) ?: error("Kunne ikke finne søknad ($id) i database")
         val feltMap = SøknadTreeWalker.mapSøknadsfelter(dbSøknad.hentSøknad())
-        LoggerFactory.getLogger(this::class.java).info(feltMap.toString())
     }
 }
