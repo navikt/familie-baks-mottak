4d3ae09e7d64587fa13f5fad2b58f81b454dc73e
Henrik Jackson <henrik.m.jackson@gmail.com>
2020-07-22 08:18:30 +0200

    Oppdater postgres passord + begynn på testing av journalførSøknad

---
+++ src/test/kotlin/no/nav/familie/ba/mottak/søknad/JournalføringTest.kt
@@ -0,0 +1,36 @@
+package no.nav.familie.ba.mottak.søknad
+
+
+import no.nav.familie.ba.mottak.DevLauncherPostgres
+import no.nav.familie.ba.mottak.søknad.domene.tilDBSøknad
+import no.nav.familie.ba.mottak.util.DbContainerInitializer
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import kotlin.test.assertEquals
+
+
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(initializers = [DbContainerInitializer::class])
+@ActiveProfiles("postgres")
+@Tag("integration")
+@SpringBootTest(classes = [DevLauncherPostgres::class])
+class JournalføringTest(
+        @Autowired
+        val journalføringService: JournalføringService) {
+
+    val søknad = SøknadTestData.søknad()
+    val testPDF = "test123".toByteArray()
+
+    @Test
+    fun `Test at journalførSøknad er riktig`() {
+        journalføringService.journalførSøknad(søknad.tilDBSøknad().id.toString(), testPDF)
+
+        assertEquals("", "")
+    }
+}
