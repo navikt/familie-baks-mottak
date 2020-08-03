e3eb5b034137a8f006efc80e779d4404c8b87a64
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-08-03 21:34:54 +0200

    Filtrering av fødselshendelser hvor det ikke finnes en relasjon til mor

--- src/main/kotlin/no/nav/familie/ba/mottak/domene/Behandling.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/domene/Behandling.kt
@@ -1,4 +1,5 @@
 package no.nav.familie.ba.mottak.domene
 
-data class NyBehandling(val søkersIdent: String,
+data class NyBehandling(val søkersIdent: String? = null,
+                        val morsIdent: String? = null,
                         val barnasIdenter: Array<String>)
