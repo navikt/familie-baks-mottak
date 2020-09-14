49582e51bfd306445989ce47ebc50d56bce0346d
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-09-14 14:54:46 +0200

    Skipper alt journalførte dokumenter

--- src/main/kotlin/no/nav/familie/ba/mottak/task/OpprettJournalføringOppgaveTask.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/task/OpprettJournalføringOppgaveTask.kt
@@ -24,32 +24,39 @@ class OpprettJournalføringOppgaveTask(private val journalpostClient: Journalpos
     override fun doTask(task: Task) {
 
         val journalpost = journalpostClient.hentJournalpost(task.payload)
-        if (journalpost.journalstatus == Journalstatus.MOTTATT) {
-            val oppgaveTypeForEksisterendeOppgave: Oppgavetype? =
-                    if (oppgaveClient.finnOppgaver(journalpost.journalpostId, Oppgavetype.Journalføring).isNotEmpty()) {
-                        Oppgavetype.Journalføring
-                    } else if (oppgaveClient.finnOppgaver(journalpost.journalpostId, Oppgavetype.Fordeling).isNotEmpty()) {
-                        Oppgavetype.Fordeling
-                    } else null
+        when (journalpost.journalstatus) {
+            Journalstatus.MOTTATT -> {
+                val oppgaveTypeForEksisterendeOppgave: Oppgavetype? =
+                        if (oppgaveClient.finnOppgaver(journalpost.journalpostId, Oppgavetype.Journalføring).isNotEmpty()) {
+                            Oppgavetype.Journalføring
+                        } else if (oppgaveClient.finnOppgaver(journalpost.journalpostId, Oppgavetype.Fordeling).isNotEmpty()) {
+                            Oppgavetype.Fordeling
+                        } else null
 
-            if (oppgaveTypeForEksisterendeOppgave == null) {
-                val nyOppgave = oppgaveClient.opprettJournalføringsoppgave(journalpost)
-                task.metadata["oppgaveId"] = "${nyOppgave.oppgaveId}"
-                taskRepository.saveAndFlush(task)
-                log.info("Oppretter ny journalførings-oppgave med id ${nyOppgave.oppgaveId} for journalpost ${journalpost.journalpostId}")
-            } else {
-                log.info("Skipper oppretting av journalførings-oppgave. Fant åpen oppgave av type $oppgaveTypeForEksisterendeOppgave for ${journalpost.journalpostId}")
+                if (oppgaveTypeForEksisterendeOppgave == null) {
+                    val nyOppgave = oppgaveClient.opprettJournalføringsoppgave(journalpost)
+                    task.metadata["oppgaveId"] = "${nyOppgave.oppgaveId}"
+                    taskRepository.saveAndFlush(task)
+                    log.info("Oppretter ny journalførings-oppgave med id ${nyOppgave.oppgaveId} for journalpost ${journalpost.journalpostId}")
+                } else {
+                    log.info("Skipper oppretting av journalførings-oppgave. Fant åpen oppgave av type $oppgaveTypeForEksisterendeOppgave for ${journalpost.journalpostId}")
+                }
             }
-        } else {
-            val error = IllegalStateException(
-                    "Journalpost ${journalpost.journalpostId} har endret status fra MOTTATT til ${journalpost.journalstatus.name}"
-            )
-            log.info("OpprettJournalføringOppgaveTask feilet.", error)
-            throw error
+
+            Journalstatus.JOURNALFOERT -> log.info("Skipper journalpost ${journalpost.journalpostId} som alt er i status JOURNALFOERT")
+
+            else -> {
+                val error = IllegalStateException(
+                        "Journalpost ${journalpost.journalpostId} har endret status fra MOTTATT til ${journalpost.journalstatus.name}"
+                )
+                log.info("OpprettJournalføringOppgaveTask feilet.", error)
+                throw error
+            }
         }
     }
 
     companion object {
+
         const val TASK_STEP_TYPE = "opprettJournalføringsoppgave"
     }
 }
