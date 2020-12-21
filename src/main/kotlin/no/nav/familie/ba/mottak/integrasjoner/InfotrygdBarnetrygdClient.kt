772af8e28942bcc47870c11951b5a7d89e5f222b
Tor Nærland <tor.nerland@nav.no>
2020-12-21 09:27:44 +0100

    Fjernet FoedselNr. Konvertert hardkodet strings til enum-verdier

--- src/main/kotlin/no/nav/familie/ba/mottak/integrasjoner/InfotrygdBarnetrygdClient.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/integrasjoner/InfotrygdBarnetrygdClient.kt
@@ -1,7 +1,5 @@
 package no.nav.familie.ba.mottak.integrasjoner
 
-import io.swagger.annotations.ApiModelProperty
-import no.nav.commons.foedselsnummer.FoedselsNr
 import no.nav.familie.ba.mottak.integrasjoner.Sakspart.*
 import no.nav.familie.http.client.AbstractRestClient
 import org.springframework.beans.factory.annotation.Qualifier
@@ -21,34 +19,29 @@ class InfotrygdBarnetrygdClient(@Value("\${FAMILIE_BA_INFOTRYGD_BARNETRYGD_API_U
 
     fun hentLøpendeUtbetalinger(søkersIdenter: List<String>, barnasIdenter: List<String>): InfotrygdSøkResponse<StønadDto> {
         return infotrygdResponseFra(
-                postRequest = { postForEntity(uri("stonad"), mapTilInfotrygdSøkRequest(søkersIdenter, barnasIdenter)) },
+                request = { postForEntity(uri("stonad"), InfotrygdSøkRequest(søkersIdenter, barnasIdenter)) },
                 onFailure = { ex -> IntegrasjonException("Feil ved søk etter stønad i infotrygd.", ex, uri("stonad")) }
         )
     }
 
     fun hentSaker(søkersIdenter: List<String>, barnasIdenter: List<String>): InfotrygdSøkResponse<SakDto> {
         return infotrygdResponseFra(
-                postRequest = { postForEntity(uri("saker"), mapTilInfotrygdSøkRequest(søkersIdenter, barnasIdenter)) },
+                request = { postForEntity(uri("saker"), InfotrygdSøkRequest(søkersIdenter, barnasIdenter)) },
                 onFailure = { ex -> IntegrasjonException("Feil ved uthenting av saker fra infotrygd.", ex, uri("saker")) }
         )
     }
 
-    private fun <T> infotrygdResponseFra(postRequest: () -> InfotrygdSøkResponse<T>,
+    private fun <T> infotrygdResponseFra(request: () -> InfotrygdSøkResponse<T>,
                                          onFailure: (Throwable) -> RuntimeException): InfotrygdSøkResponse<T> {
         return if (environment.activeProfiles.contains("e2e"))  InfotrygdSøkResponse(emptyList(), emptyList())
-        else runCatching(postRequest).getOrElse { throw onFailure(it) }
+        else runCatching(request).getOrElse { throw onFailure(it) }
     }
 
     private fun uri(endepunkt: String) = URI.create("$clientUri/$endepunkt")
-
-    private fun mapTilInfotrygdSøkRequest(søkersIdenter: List<String>,
-                                          barnasIdenter: List<String>): InfotrygdSøkRequest {
-        return InfotrygdSøkRequest(søkersIdenter.map { FoedselsNr(it) }, barnasIdenter.map { FoedselsNr(it) })
-    }
 }
 
-data class InfotrygdSøkRequest(val brukere: List<FoedselsNr>,
-                               val barn: List<FoedselsNr>? = null)
+data class InfotrygdSøkRequest(val brukere: List<String>,
+                               val barn: List<String>? = null)
 
 data class InfotrygdSøkResponse<T> (
         val bruker: List<T>,
@@ -65,8 +58,8 @@ val InfotrygdSøkResponse<SakDto>.resultat: Sakspart? @JvmName("getSakspart")
 private fun List<SakDto>.harSak(): Boolean {
     val (sakerMedVedtak, sakerUtenVedtak) = this.partition { it.vedtak != null }
 
-    return sakerMedVedtak.let { it.all { it.vedtak!!.opphørsgrunn != "M" } && it.isNotEmpty() }
-           || sakerUtenVedtak.any { it.status != "FB" }
+    return sakerMedVedtak.let { it.all { it.vedtak!!.opphørsgrunn != Opphørsgrunn.MIGRERT.kode } && it.isNotEmpty() }
+           || sakerUtenVedtak.any { it.status != StatusKode.FB.name }
 }
 
 data class StønadDto(
@@ -88,27 +81,25 @@ data class SakDto(
         val regDato: LocalDate? = null,
         val RegAvEnhet: String? = null,
         val BehenEnhet: String? = null,
-
-
-        @ApiModelProperty(notes = """
-        IP: - Saksbehandlingen kan starte med Statuskode IP (Ikke påbegynt). Da er det kun registrert en sakslinje uten at vedtaksbehandling er startet.
-        UB: - Saksbehandling startet - når sak med status UB - Under Behandling - lagres, rapporteres hendelsen BehandlingOpprettet
-        SG: - Saksbehandler 1 har fullført og sendt til saksbehandler 2 for godkjenning
-        UK: - Underkjent av saksbehandler 2 med retur til saksbehandler 1
-        FB: - FerdigBehandlet
-        FI: - ferdig iverksatt
-        RF: - returnert feilsendt
-        RM: - returnert midlertidig
-        RT: - returnert til
-        ST: - sendt til
-        VD: - videresendt Direktoratet
-        VI: - venter på iverksetting
-        VT: - videresendt Trygderetten
-        
-        Kolonne: S15_STATUS.
-    """,
-                          allowableValues = "IP,UB,SG,UK,FB,FI,RF,RM,RT,ST,VD,VI,VT"
-        )
         val status: String,         // S15_STATUS
-
 )
+
+enum class Opphørsgrunn(val kode: String) {
+    MIGRERT("5")
+}
+
+enum class StatusKode(val beskrivelse: String) {
+    IP("Saksbehandlingen kan starte med Statuskode IP (Ikke påbegynt). Da er det kun registrert en sakslinje uten at vedtaksbehandling er startet."),
+    UB("Saksbehandling startet - når sak med status UB - Under Behandling - lagres, rapporteres hendelsen BehandlingOpprettet"),
+    SG("Saksbehandler 1 har fullført og sendt til saksbehandler 2 for godkjenning"),
+    UK("Underkjent av saksbehandler 2 med retur til saksbehandler 1"),
+    FB("FerdigBehandlet"),
+    FI("ferdig iverksatt"),
+    RF("returnert feilsendt"),
+    RM("returnert midlertidig"),
+    RT("returnert til"),
+    ST("sendt til"),
+    VD("videresendt Direktoratet"),
+    VI("venter på iverksetting"),
+    VT("videresendt Trygderetten"),
+}
