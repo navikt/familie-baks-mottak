d4cf06dd8f79aa2f247d08901985ead98fd0c562
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-01-18 12:52:26 +0100

    Logikk for vurder livshendelse v1

--- src/main/kotlin/no/nav/familie/ba/mottak/task/VurderLivshendelseTask.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/task/VurderLivshendelseTask.kt
@@ -1,6 +1,6 @@
 package no.nav.familie.ba.mottak.task
 
-import no.nav.familie.ba.mottak.integrasjoner.OppgaveClient
+import no.nav.familie.ba.mottak.integrasjoner.*
 import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
@@ -12,14 +12,39 @@ import org.springframework.stereotype.Service
 
 @Service
 @TaskStepBeskrivelse(taskStepType = VurderLivshendelseTask.TASK_STEP_TYPE, beskrivelse = "Vurder livshendelse")
-class VurderLivshendelseTask(private val oppgaveClient: OppgaveClient,
-                             private val taskRepository: TaskRepository) : AsyncTaskStep {
+class VurderLivshendelseTask(
+    private val oppgaveClient: OppgaveClient,
+    private val taskRepository: TaskRepository,
+    private val pdlClient: PdlClient,
+    private val sakClient: SakClient,
+) : AsyncTaskStep {
 
     val log: Logger = LoggerFactory.getLogger(OpprettBehandleSakOppgaveTask::class.java)
 
     override fun doTask(task: Task) {
         val payload = objectMapper.readValue(task.payload, VurderLivshendelseTaskDTO::class.java)
         //hent familierelasjoner
+        val pdlPersonData = pdlClient.hentPerson(payload.personIdent, "hentperson-relasjon-dødsfall")
+        val familierelasjon = pdlPersonData.familierelasjoner
+        if (pdlPersonData.dødsfall?.first().dødsdato != null) {
+            //Skal man gjøre spesifikk filtrering med OR for å sikre at det ikke kommer en ny relasjonstype
+            val listeMedBarn = familierelasjon.filter { it.minRolleForPerson != Familierelasjonsrolle.BARN }.map { it.relatertPersonsIdent }
+            if (listeMedBarn.isNotEmpty()) {
+                val sak = sakClient.hentPågåendeSakStatus(payload.personIdent, listeMedBarn)
+                if (sak.baSak == Sakspart.SØKER) {
+                    oppgaveClient.opprettVurderLivshendelseOppgave(payload.personIdent, "Søker har aktiv sak")
+                }
+            }
+
+            val listeMedForeldre = familierelasjon.filter { it.minRolleForPerson == Familierelasjonsrolle.BARN }.map { it.relatertPersonsIdent }
+
+            listeMedForeldre.forEach {
+                val sak = sakClient.hentPågåendeSakStatus(it, listOf(payload.personIdent))
+                if (sak.baSak == Sakspart.SØKER) {
+                    oppgaveClient.opprettVurderLivshendelseOppgave(it, "Barn har aktiv sak")
+                }
+            }
+        }
 
 
         //val oppgaver = oppgaveClient.finnOppgaver(journalpost.journalpostId, null)
@@ -28,6 +53,7 @@ class VurderLivshendelseTask(private val oppgaveClient: OppgaveClient,
     }
 
     companion object {
+
         const val TASK_STEP_TYPE = "opprettBehandleSakoppgave"
     }
 }
