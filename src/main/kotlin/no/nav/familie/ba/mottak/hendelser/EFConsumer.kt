77d5d9312c2f713b3bf9033fac00127c1afd9849
tnarland <47184872+tnarland@users.noreply.github.com>
2021-02-23 12:34:03 +0100

    setter autoOffsetReset = earliest (#262)

---
+++ src/main/kotlin/no/nav/familie/ba/mottak/hendelser/EFConsumer.kt
@@ -0,0 +1,28 @@
+package no.nav.familie.ba.mottak.hendelser
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Service
+
+@Service
+@Profile("!e2e")
+class EFConsumer {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+    private val secureLogger = LoggerFactory.getLogger("secureLogger")
+
+    @KafkaListener(topics = ["aapen-ef-overgangstonad-v1"],
+                   id = "ef-overgangstonad",
+                   idIsGroup = false,
+                   containerFactory = "kafkaEFListenerContainerFactory")
+    fun listen(cr: ConsumerRecord<String, String>, ack: Acknowledgment) {
+        val melding = cr.value()
+
+        logger.info("Melding fra EF. Key: ", cr.key())
+        secureLogger.info("Melding fra EF. Key : " + cr.key() + ", Value: " + melding)
+
+        ack.acknowledge()
+    }
+}
