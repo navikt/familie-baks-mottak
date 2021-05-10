37d6c60717983563c100f8b421ccb57308773384
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-05-10 11:08:11 +0200

    (test) Fjern protection fra taImotSøknad i preprod

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/SøknadController.kt
@@ -3,8 +3,9 @@ package no.nav.familie.ba.mottak.søknad
 import no.nav.familie.kontrakter.ba.søknad.Søknad
 import no.nav.familie.ba.mottak.søknad.domene.FødselsnummerErNullException
 import no.nav.familie.kontrakter.felles.Ressurs
-import no.nav.security.token.support.core.api.Protected
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.familie.ba.mottak.config.FeatureToggleService
+import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
@@ -15,12 +16,13 @@ import java.time.LocalDateTime
 
 @RestController
 @RequestMapping(path = ["/api"], produces = [APPLICATION_JSON_VALUE])
-@Protected
+@ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
 class SøknadController(private val featureToggleService: FeatureToggleService,
                        private val søknadService: SøknadService) {
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
 
     @PostMapping(value = ["/soknad"], consumes = [MULTIPART_FORM_DATA_VALUE])
+    @Unprotected
     fun taImotSøknad(@RequestPart("søknad") søknad: Søknad): ResponseEntity<Ressurs<Kvittering>> {
         val lagreSøknad = featureToggleService.isEnabled("familie-ba-mottak.lagre-soknad")
         log.info("Lagring av søknad = $lagreSøknad")
