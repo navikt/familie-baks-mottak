4df557966afaa4626f2a5a839f130dd089a27075
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-05-27 15:18:29 +0200

    Revert "(tmp) Ekstra logging ved dokgen-feil [e2e skip]"
    This reverts commit 0f107ef0e73cb3513e742db132546bd79d7be483.

--- src/main/kotlin/no/nav/familie/ba/mottak/integrasjoner/PdfClient.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/integrasjoner/PdfClient.kt
@@ -1,13 +1,10 @@
 package no.nav.familie.ba.mottak.integrasjoner
 
 import no.nav.familie.http.client.AbstractRestClient
-import no.nav.familie.kontrakter.felles.objectMapper
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestOperations
-import java.lang.RuntimeException
 import java.net.URI
 
 @Service
@@ -15,16 +12,6 @@ class PdfClient(@Qualifier("restTemplateUnsecured") operations: RestOperations,
                 @Value("\${FAMILIE_BA_DOKGEN_API_URL}") private val dokgenUri: String) : AbstractRestClient(operations, "pdf") {
     fun lagPdf(labelValueJson: Map<String, Any>): ByteArray {
         val sendInnUri = URI.create("$dokgenUri/template/soknad/download-pdf")
-        try {
-            return postForEntity(sendInnUri, labelValueJson)
-        } catch (e: RuntimeException) {
-            val asString = objectMapper.writeValueAsString(labelValueJson)
-            logger.info(asString)
-            throw e
-        }
-    }
-
-    companion object {
-        val logger = LoggerFactory.getLogger(this::class.java)
+        return postForEntity(sendInnUri, labelValueJson)
     }
 }
