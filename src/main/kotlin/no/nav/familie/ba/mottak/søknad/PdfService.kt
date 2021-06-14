0625ccd14da75d7e7ac6b7c21ddaf97af4b2196a
Stig Strøm <stig.helge.strom@nav.no>
2021-06-14 12:05:35 +0200

    Skrur opp minne for java  e2e skip (#337)

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/PdfService.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/PdfService.kt
@@ -5,6 +5,8 @@ import no.nav.familie.ba.mottak.integrasjoner.PdfClient
 import org.springframework.stereotype.Service
 import no.nav.familie.ba.mottak.søknad.domene.DBSøknad
 import no.nav.familie.kontrakter.felles.objectMapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.*
@@ -13,7 +15,10 @@ import java.util.*
 @Service
 class PdfService(private val søknadRepository: SøknadRepository, private val pdfClient: PdfClient) {
 
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+
     fun lagPdf(id: String): ByteArray {
+        log.info("Prøver å hente søknadspdf for $id")
         val dbSøknad = søknadRepository.hentDBSøknad(id.toLong()) ?: error("Kunne ikke finne søknad ($id) i database")
         val feltMap = objectMapper.convertValue(dbSøknad.hentSøknad(), object:TypeReference<Map<String,Any>>() {})
         val utvidetFeltMap = feltMap + hentEkstraFelter(dbSøknad)
--- src/main/kotlin/no/nav/familie/ba/mottak/task/JournalførSøknadTask.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/task/JournalførSøknadTask.kt
@@ -15,14 +15,21 @@ class JournalførSøknadTask(private val pdfService: PdfService,
                            private val journalføringService: JournalføringService) : AsyncTaskStep {
 
     override fun doTask(task: Task) {
-        log.info("Generer pdf og journalfør søknad")
-        val pdf = pdfService.lagPdf(task.payload)
-        journalføringService.journalførSøknad(task.payload, pdf)
+        try {
+            log.info("Generer pdf og journalfør søknad")
+            val pdf = pdfService.lagPdf(task.payload)
+            journalføringService.journalførSøknad(task.payload, pdf)
+        } catch (e: Exception) {
+            log.error("Uventet feil ved journalføring av søknad. taskId=${task.id}. Se task eller securelog")
+            SECURE_LOGGER.error("Uventet feil ved journalføring søknad $task", e)
+            throw e
+        }
     }
 
     companion object {
         const val JOURNALFØR_SØKNAD = "journalførSøknad"
         val log: Logger = LoggerFactory.getLogger(this::class.java)
+        val SECURE_LOGGER: Logger = LoggerFactory.getLogger("secureLogger")
     }
 
 }
