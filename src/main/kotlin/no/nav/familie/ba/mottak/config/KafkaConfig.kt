88ea13ff4c09d6e7a17e0084f7fdc98db930691d
Stig Strøm <stig.helge.strom@nav.no>
2021-08-12 14:21:01 +0200

    Lagt til SecureloggingErrorHandler for kafka og tatt den i bruk for preprod (#377)

--- src/main/kotlin/no/nav/familie/ba/mottak/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/config/KafkaConfig.kt
@@ -2,14 +2,17 @@ package no.nav.familie.ba.mottak.config
 
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import org.apache.avro.generic.GenericRecord
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.listener.ContainerProperties
+import org.springframework.kafka.listener.ErrorHandler
 import java.time.Duration
 
 @EnableKafka
@@ -17,16 +20,26 @@ import java.time.Duration
 @Profile("!e2e")
 class KafkaConfig {
 
+
+
     @Bean
-    fun kafkaLeesahListenerContainerFactory(properties: KafkaProperties, kafkaErrorHandler: KafkaErrorHandler)
+    fun kafkaLeesahListenerContainerFactory(properties: KafkaProperties, kafkaErrorHandler: KafkaErrorHandler, environment: Environment)
             : ConcurrentKafkaListenerContainerFactory<Int, GenericRecord> {
         val factory = ConcurrentKafkaListenerContainerFactory<Int, GenericRecord>()
         factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
         factory.containerProperties.authorizationExceptionRetryInterval = Duration.ofSeconds(2)
         factory.consumerFactory = DefaultKafkaConsumerFactory(properties.buildConsumerProperties())
-        factory.setErrorHandler(kafkaErrorHandler)
+        if (environment.activeProfiles.contains("preprod")) {
+            factory.setErrorHandler(KafkaSecureloggingErrorHandler())
+        } else {
+            factory.setErrorHandler(kafkaErrorHandler)
+        }
         return factory
     }
+
+
+
+
 
     @Bean
     fun kafkaJournalføringHendelseListenerContainerFactory(properties: KafkaProperties, kafkaErrorHandler: KafkaErrorHandler)
