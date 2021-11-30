6423e9290f0e9776c80b03c263ce0c468f6aeac7
norafutsaeter <nora.futseter@nav.no>
2021-11-30 12:49:42 +0100

    Legg til støtte for søknad v6

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/ArkiverDokumentRequestMapper.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/ArkiverDokumentRequestMapper.kt
@@ -4,7 +4,8 @@ import no.nav.familie.ba.mottak.søknad.domene.DBSøknad
 import no.nav.familie.ba.mottak.søknad.domene.DBVedlegg
 import no.nav.familie.kontrakter.ba.søknad.v4.Søknadstype
 import no.nav.familie.kontrakter.ba.søknad.v4.Dokumentasjonsbehov
-import no.nav.familie.kontrakter.ba.søknad.v5.Søknad
+import no.nav.familie.kontrakter.ba.søknad.v5.Søknad as SøknadV5
+import no.nav.familie.kontrakter.ba.søknad.v6.Søknad
 import no.nav.familie.kontrakter.felles.dokarkiv.v2.Dokument
 import no.nav.familie.kontrakter.felles.dokarkiv.v2.Filtype
 import no.nav.familie.kontrakter.felles.dokarkiv.v2.ArkiverDokumentRequest
@@ -12,7 +13,12 @@ import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
 
 object ArkiverDokumentRequestMapper {
 
-    fun toDto(dbSøknad: DBSøknad, pdf: ByteArray, vedleggMap: Map<String, DBVedlegg>, pdfOriginalSpråk: ByteArray): ArkiverDokumentRequest {
+    fun toDto(
+        dbSøknad: DBSøknad,
+        pdf: ByteArray,
+        vedleggMap: Map<String, DBVedlegg>,
+        pdfOriginalSpråk: ByteArray
+    ): ArkiverDokumentRequest {
         val søknadstype = dbSøknad.hentSøknad().søknadstype
 
         val dokumenttype = when (søknadstype) {
@@ -22,27 +28,36 @@ object ArkiverDokumentRequestMapper {
         }
 
         val søknadsdokumentJson =
-                Dokument(
-                    dokument = dbSøknad.søknadJson.toByteArray(),
-                    filtype = Filtype.JSON,
-                    filnavn = null,
-                    tittel = "SØKNAD_${dokumenttype}_JSON",
-                    dokumenttype = dokumenttype)
+            Dokument(
+                dokument = dbSøknad.søknadJson.toByteArray(),
+                filtype = Filtype.JSON,
+                filnavn = null,
+                tittel = "SØKNAD_${dokumenttype}_JSON",
+                dokumenttype = dokumenttype
+            )
         val søknadsdokumentPdf =
-                Dokument(
-                    dokument = pdf,
-                    filtype = Filtype.PDFA,
-                    filnavn = null,
-                    tittel = when (dokumenttype) {
-                        Dokumenttype.BARNETRYGD_UTVIDET -> "Søknad om utvidet barnetrygd"
-                        Dokumenttype.BARNETRYGD_ORDINÆR -> "Søknad om ordinær barnetrygd"
-                        else -> "Søknad om ordinær barnetrygd"
-                    },
-                    dokumenttype = dokumenttype
-                )
+            Dokument(
+                dokument = pdf,
+                filtype = Filtype.PDFA,
+                filnavn = null,
+                tittel = when (dokumenttype) {
+                    Dokumenttype.BARNETRYGD_UTVIDET -> "Søknad om utvidet barnetrygd"
+                    Dokumenttype.BARNETRYGD_ORDINÆR -> "Søknad om ordinær barnetrygd"
+                    else -> "Søknad om ordinær barnetrygd"
+                },
+                dokumenttype = dokumenttype
+            )
         val hoveddokumentvarianter = listOf(søknadsdokumentPdf, søknadsdokumentJson)
 
-        return ArkiverDokumentRequest(
+        return if (dbSøknad.hentSøknadVersjon() == "v5")
+            ArkiverDokumentRequest(
+                fnr = dbSøknad.fnr,
+                forsøkFerdigstill = false,
+                hoveddokumentvarianter = hoveddokumentvarianter,
+                vedleggsdokumenter = hentVedleggListeTilArkivering(dbSøknad.hentSøknadV5(), vedleggMap, pdfOriginalSpråk),
+                eksternReferanseId = dbSøknad.id.toString()
+            )
+        else ArkiverDokumentRequest(
             fnr = dbSøknad.fnr,
             forsøkFerdigstill = false,
             hoveddokumentvarianter = hoveddokumentvarianter,
@@ -51,11 +66,15 @@ object ArkiverDokumentRequestMapper {
         )
     }
 
-    private fun hentVedleggListeTilArkivering(søknad: Søknad, vedleggMap: Map<String, DBVedlegg>, pdfOriginalSpråk: ByteArray): List<Dokument> {
+    private fun hentVedleggListeTilArkivering(
+        søknad: Søknad,
+        vedleggMap: Map<String, DBVedlegg>,
+        pdfOriginalSpråk: ByteArray
+    ): List<Dokument> {
         val vedlegg = mutableListOf<Dokument>()
 
-        søknad.dokumentasjon.forEach{ dokumentasjonskrav ->
-            dokumentasjonskrav.opplastedeVedlegg.forEach{ opplastaVedlegg ->
+        søknad.dokumentasjon.forEach { dokumentasjonskrav ->
+            dokumentasjonskrav.opplastedeVedlegg.forEach { opplastaVedlegg ->
                 vedleggMap.get(opplastaVedlegg.dokumentId)?.also { dbFil ->
                     vedlegg.add(
                         Dokument(
@@ -72,18 +91,55 @@ object ArkiverDokumentRequestMapper {
         if (pdfOriginalSpråk.isNotEmpty()) {
             vedlegg.add(
                 Dokument(
-                dokument = pdfOriginalSpråk,
-                dokumenttype = Dokumenttype.BARNETRYGD_VEDLEGG,
-                filtype = Filtype.PDFA,
-                tittel = "Søknad på originalt utfylt språk"
-            ))
+                    dokument = pdfOriginalSpråk,
+                    dokumenttype = Dokumenttype.BARNETRYGD_VEDLEGG,
+                    filtype = Filtype.PDFA,
+                    tittel = "Søknad på originalt utfylt språk"
+                )
+            )
+        }
+
+        return vedlegg
+    }
+
+    private fun hentVedleggListeTilArkivering(
+        søknad: SøknadV5,
+        vedleggMap: Map<String, DBVedlegg>,
+        pdfOriginalSpråk: ByteArray
+    ): List<Dokument> {
+        val vedlegg = mutableListOf<Dokument>()
+
+        søknad.dokumentasjon.forEach { dokumentasjonskrav ->
+            dokumentasjonskrav.opplastedeVedlegg.forEach { opplastaVedlegg ->
+                vedleggMap.get(opplastaVedlegg.dokumentId)?.also { dbFil ->
+                    vedlegg.add(
+                        Dokument(
+                            dokument = dbFil.data,
+                            dokumenttype = Dokumenttype.BARNETRYGD_VEDLEGG,
+                            filtype = Filtype.PDFA,
+                            tittel = dokumentasjonsbehovTilTittel(opplastaVedlegg.tittel)
+                        )
+                    )
+                }
+            }
+        }
+
+        if (pdfOriginalSpråk.isNotEmpty()) {
+            vedlegg.add(
+                Dokument(
+                    dokument = pdfOriginalSpråk,
+                    dokumenttype = Dokumenttype.BARNETRYGD_VEDLEGG,
+                    filtype = Filtype.PDFA,
+                    tittel = "Søknad på originalt utfylt språk"
+                )
+            )
         }
 
         return vedlegg
     }
 
     private fun dokumentasjonsbehovTilTittel(dokumentasjonsbehov: Dokumentasjonsbehov): String {
-        return when(dokumentasjonsbehov) {
+        return when (dokumentasjonsbehov) {
             Dokumentasjonsbehov.ADOPSJON_DATO -> "Adopsjonsdato"
             Dokumentasjonsbehov.AVTALE_DELT_BOSTED -> "Avtale om delt bosted"
             Dokumentasjonsbehov.VEDTAK_OPPHOLDSTILLATELSE -> "Vedtak om oppholdstillatelse"
