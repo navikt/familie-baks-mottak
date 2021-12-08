97eeccf4a425bfb03db5d6000ed0a3097d7f16cf
Henning Solberg <henning.solberg@nav.no>
2021-12-08 12:28:50 +0100

    Lagt inn støtte for å spole igjennom alle journalføringshendelser på aiven + gjort klart for neste steg som er å lagre og acke hendelsene.

--- src/main/kotlin/no/nav/familie/ba/mottak/hendelser/JournalføringHendelseAivenConsumer.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/hendelser/JournalføringHendelseAivenConsumer.kt
@@ -35,9 +35,10 @@ class JournalføringHendelseAivenConsumer(val journalhendelseService: Journalhen
     @Transactional
     fun listen(consumerRecord: ConsumerRecord<Long, JournalfoeringHendelseRecord>, ack: Acknowledgment) {
         try {
-            logger.info("Journalføringshendelse fra aiven mottatt. Offset: ${consumerRecord.offset()}")
+            //logger.info("Journalføringshendelse fra aiven mottatt. Offset: ${consumerRecord.offset()}")
             //secureLogger.info("Journalføringshendelse fra aiven mottatt. Offset: ${consumerRecord.offset()} Key: ${consumerRecord.key()} Value: ${consumerRecord.value()}")
             //journalhendelseService.prosesserNyHendelse(consumerRecord, ack)
+            //journalhendelseService.bareLagreLoggOgAckAiven(consumerRecord, ack)
             ack.acknowledge()
         } catch (e: Exception) {
             journalføringshendelseAivenConsumerFeilCounter.increment()
