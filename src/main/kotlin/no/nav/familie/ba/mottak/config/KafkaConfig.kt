a41550d7f83aec0e823f780f9ccad8f367210769
Tor Nærland <tor.nerland@nav.no>
2022-01-27 14:21:26 +0100

    setter JournalføringHendelseListener til latest

--- src/main/kotlin/no/nav/familie/ba/mottak/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/config/KafkaConfig.kt
@@ -71,7 +71,11 @@ class KafkaConfig {
         val factory = ConcurrentKafkaListenerContainerFactory<Long, JournalfoeringHendelseRecord>()
         factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
         factory.containerProperties.authorizationExceptionRetryInterval = Duration.ofSeconds(2)
-        factory.consumerFactory = DefaultKafkaConsumerFactory(properties.buildConsumerProperties())
+        factory.consumerFactory = DefaultKafkaConsumerFactory(
+            properties.buildConsumerProperties().also {
+                it[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = OffsetResetStrategy.LATEST.toString().lowercase()
+            }
+        )
         factory.setErrorHandler(kafkaErrorHandler)
         return factory
     }
