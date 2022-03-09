40fde93ed5c5cd409d18a9280bb31e269570047d
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-03-09 13:23:55 +0100

    Flytter consumer config til annotering

--- src/main/kotlin/no/nav/familie/ba/mottak/hendelser/EnsligForsørgerInfotrygdHendelseConsumer.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/hendelser/EnsligForsørgerInfotrygdHendelseConsumer.kt
@@ -58,6 +58,7 @@ class EnsligForsørgerInfotrygdHendelseConsumer(val vedtakOmOvergangsstønadServ
     val ensligForsørgerInfotrygdHendelseConsumerFeilCounter: Counter = Metrics.counter("ef.hendelse.infotrygdvedtak.feil")
 
     @KafkaListener(
+        groupId = "ba-mottak-ef-infotrygd-1",
         id = "ef-infotrygd-overgangstonad",
         topics = ["teamfamilie.$TOPIC_INFOTRYGD_VEDTAK"],
         containerFactory = "kafkaEnsligForsørgerInfotrygdHendelseContainerFactory",
