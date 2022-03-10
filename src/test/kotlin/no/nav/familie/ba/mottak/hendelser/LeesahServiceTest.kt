1c703b29f62b8737edcd237501b32ca2265ab366
tnarland <47184872+tnarland@users.noreply.github.com>
2022-03-10 09:37:01 +0100

    Update LeesahServiceTest.kt

--- src/test/kotlin/no/nav/familie/ba/mottak/hendelser/LeesahServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ba/mottak/hendelser/LeesahServiceTest.kt
@@ -115,7 +115,7 @@ class LeesahServiceTest {
         }
         assertThat(taskSlot.captured).isNotNull
         assertThat(taskSlot.captured.payload)
-            .isEqualTo("{\"personIdent\":\"12345678901\",\"type\":\"SIVILSTAND\",\"gyldigFom\":\"2022-02-22\"}")
+            .isEqualTo("{\"personIdent\":\"12345678901\",\"type\":\"SIVILSTAND\"}")
         assertThat(taskSlot.captured.taskStepType).isEqualTo(VurderLivshendelseTask.TASK_STEP_TYPE)
 
         verify(exactly = 2) {
