3e7cafab0f0d5baf211d020999caa3154f71ccb7
Stig Strøm <stig.helge.strom@nav.no>
2022-03-15 14:45:27 +0100

    Fjerner sjekk av FNR mot TPS fordi TPS skal være sanntidsoppdatert fra PDL (#562)
    * Fjerner sjekk av FNR mot TPS fordi TPS skal være sanntidsoppdatert fra PDL

--- src/main/kotlin/no/nav/familie/ba/mottak/task/MottaFødselshendelseTask.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/task/MottaFødselshendelseTask.kt
@@ -9,37 +9,33 @@ import no.nav.familie.ba.mottak.domene.personopplysning.PersonIdent
 import no.nav.familie.ba.mottak.domene.personopplysning.harAdresseGradering
 import no.nav.familie.ba.mottak.domene.personopplysning.harBostedsadresse
 import no.nav.familie.ba.mottak.integrasjoner.PdlClient
-import no.nav.familie.ba.mottak.integrasjoner.TPSPersonClient
 import no.nav.familie.ba.mottak.util.erBostNummer
 import no.nav.familie.ba.mottak.util.erDnummer
 import no.nav.familie.ba.mottak.util.erFDatnummer
-import no.nav.familie.ba.mottak.util.nesteGyldigeTriggertidFødselshendelser
 import no.nav.familie.kontrakter.felles.personopplysning.FORELDERBARNRELASJONROLLE
 import no.nav.familie.kontrakter.felles.personopplysning.ForelderBarnRelasjon
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
-import no.nav.familie.prosessering.error.RekjørSenereException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
-import org.springframework.web.client.HttpClientErrorException
 
 
 @Service
-@TaskStepBeskrivelse(taskStepType = MottaFødselshendelseTask.TASK_STEP_TYPE,
-                     beskrivelse = "Motta fødselshendelse",
-                     maxAntallFeil = 10,
-                     triggerTidVedFeilISekunder = 60L * 60 * 24)
-class MottaFødselshendelseTask(private val taskRepository: TaskRepository,
-                               private val tpsPersonClient: TPSPersonClient,
-                               private val environment: Environment,
-                               private val pdlClient: PdlClient,
-                               @Value("\${FØDSELSHENDELSE_REKJØRINGSINTERVALL_MINUTTER}") private val rekjøringsintervall: Long)
-    : AsyncTaskStep {
+@TaskStepBeskrivelse(
+    taskStepType = MottaFødselshendelseTask.TASK_STEP_TYPE,
+    beskrivelse = "Motta fødselshendelse",
+    maxAntallFeil = 3
+)
+class MottaFødselshendelseTask(
+    private val taskRepository: TaskRepository,
+    private val pdlClient: PdlClient,
+    @Value("\${FØDSELSHENDELSE_REKJØRINGSINTERVALL_MINUTTER}") private val rekjøringsintervall: Long
+) : AsyncTaskStep {
 
     val log: Logger = LoggerFactory.getLogger(MottaFødselshendelseTask::class.java)
     val barnHarDnrCounter: Counter = Metrics.counter("barnetrygd.hendelse.ignorert.barn.har.dnr.eller.fdatnr")
@@ -59,10 +55,6 @@ class MottaFødselshendelseTask(private val taskRepository: TaskRepository,
         try {
             val personMedRelasjoner = pdlClient.hentPersonMedRelasjoner(barnetsId)
 
-            // denne kreves for at infotrygd ikke skal få fødselshendelser som ikke ligger i TPS
-            // vil feile ved forsinkelse i TPS feks i helger og helligdager.
-            // Kan fjernes når barnetrygd er ute av infotrygd
-            tpsPersonClient.hentPersonMedRelasjoner(barnetsId)
 
             val morsIdent = hentMor(personMedRelasjoner)
 
@@ -83,10 +75,14 @@ class MottaFødselshendelseTask(private val taskRepository: TaskRepository,
 
                 task.metadata["morsIdent"] = morsIdent.id
                 val nesteTask = Task(
-                        SendTilSakTask.TASK_STEP_TYPE,
-                        jacksonObjectMapper().writeValueAsString(NyBehandling(morsIdent = morsIdent.id,
-                                                                              barnasIdenter = arrayOf(barnetsId))),
-                        task.metadata
+                    SendTilSakTask.TASK_STEP_TYPE,
+                    jacksonObjectMapper().writeValueAsString(
+                        NyBehandling(
+                            morsIdent = morsIdent.id,
+                            barnasIdenter = arrayOf(barnetsId)
+                        )
+                    ),
+                    task.metadata
                 )
 
                 taskRepository.save(nesteTask)
@@ -94,12 +90,6 @@ class MottaFødselshendelseTask(private val taskRepository: TaskRepository,
                 log.info("Skipper fødselshendelse fordi man ikke fant en mor")
             }
         } catch (ex: RuntimeException) {
-            if (ex is HttpClientErrorException.NotFound) {
-                throw RekjørSenereException(
-                    årsak = "MottaFødselshendelseTask feilet",
-                    triggerTid = nesteGyldigeTriggertidFødselshendelser(rekjøringsintervall, environment)
-                )
-            }
             throw ex
         }
     }
