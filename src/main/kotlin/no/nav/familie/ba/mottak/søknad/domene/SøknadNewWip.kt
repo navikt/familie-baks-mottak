0551a6df38c41ff7ff1f24e4f9b4611e1681425d
norafutsaeter <nora.futseter@nav.no>
2022-03-17 15:29:04 +0100

    kommentar

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/domene/SøknadNewWip.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/domene/SøknadNewWip.kt
@@ -110,6 +110,7 @@ data class AndreForelder(
     val andreUtbetalingsperioder: List<Søknadsfelt<Utbetalingsperiode>> = listOf()
 )
 
+// EØS
 data class Omsorgsperson(
     val omsorgspersonNavn: Søknadsfelt<String>,
     val omsorgspersonSlektsforhold: Søknadsfelt<String>,
