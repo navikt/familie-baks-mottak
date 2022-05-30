de659cbb04cf818177c0347605058b3260e2a472
Daphne Leebeek <daphne.leebeek@nav.no>
2022-05-30 11:04:42 +0200

    oppdater v8 wip-kontrakt

--- src/main/kotlin/no/nav/familie/ba/mottak/søknad/domene/SøknadWipV8.kt
+++ src/main/kotlin/no/nav/familie/ba/mottak/søknad/domene/SøknadWipV8.kt
@@ -1,16 +1,16 @@
-package no.nav.familie.ba.soknad.api.controllers
+package no.nav.familie.ba.mottak.søknad.domene
+
 import no.nav.familie.kontrakter.ba.søknad.SIVILSTANDTYPE
 import no.nav.familie.kontrakter.ba.søknad.SøknadAdresse
 import no.nav.familie.kontrakter.ba.søknad.v4.Locale
 import no.nav.familie.kontrakter.ba.søknad.v4.NåværendeSamboer
 import no.nav.familie.kontrakter.ba.søknad.v4.SpørsmålId
-import no.nav.familie.kontrakter.ba.søknad.v4.Søknaddokumentasjon
 import no.nav.familie.kontrakter.ba.søknad.v4.Søknadsfelt
 import no.nav.familie.kontrakter.ba.søknad.v4.Søknadstype
 import no.nav.familie.kontrakter.ba.søknad.v4.TidligereSamboer
 import no.nav.familie.kontrakter.ba.søknad.v4.Utenlandsopphold
 import no.nav.familie.kontrakter.ba.søknad.v5.RegistrertBostedType
-import no.nav.familie.kontrakter.ba.søknad.v7.AndreForelder
+import no.nav.familie.kontrakter.ba.søknad.v6.AndreForelderUtvidet
 import no.nav.familie.kontrakter.ba.søknad.v7.Arbeidsperiode
 import no.nav.familie.kontrakter.ba.søknad.v7.EøsBarnetrygdsperiode
 import no.nav.familie.kontrakter.ba.søknad.v7.IdNummer
@@ -34,13 +34,12 @@ data class Søker(
     val ident: Søknadsfelt<String>,
     val navn: Søknadsfelt<String>,
     val statsborgerskap: Søknadsfelt<List<String>>,
-    // Ny
-    val adresse: Søknadsfelt<SøknadAdresse>? = null,
-
+    val adresse: Søknadsfelt<SøknadAdresse?>,
+    val adressebeskyttelse: Boolean,
     val sivilstand: Søknadsfelt<SIVILSTANDTYPE>,
     val spørsmål: Map<String, Søknadsfelt<Any>>,
-    val nåværendeSamboer: Søknadsfelt<NåværendeSamboer>?,
-    val tidligereSamboere: List<Søknadsfelt<TidligereSamboer>>,
+    val nåværendeSamboer: Søknadsfelt<NåværendeSamboer>? = null,
+    val tidligereSamboere: List<Søknadsfelt<TidligereSamboer>> = listOf(),
     val utenlandsperioder: List<Søknadsfelt<Utenlandsopphold>> = listOf(),
     val andreUtbetalingsperioder: List<Søknadsfelt<Utbetalingsperiode>> = listOf(),
     val arbeidsperioderUtland: List<Søknadsfelt<Arbeidsperiode>> = listOf(),
@@ -55,7 +54,7 @@ data class Barn(
     val ident: Søknadsfelt<String>,
     val navn: Søknadsfelt<String>,
     val registrertBostedType: Søknadsfelt<RegistrertBostedType>,
-    val alder: Søknadsfelt<String>,
+    val alder: Søknadsfelt<String>? = null,
     val spørsmål: Map<String, Søknadsfelt<Any>>,
     val utenlandsperioder: List<Søknadsfelt<Utenlandsopphold>> = listOf(),
     val andreForelder: AndreForelder? = null,
@@ -67,16 +66,70 @@ data class Barn(
 data class Omsorgsperson(
     val navn: Søknadsfelt<String>,
     val slektsforhold: Søknadsfelt<String>,
-    val slektsforholdSpesifisering: Søknadsfelt<String>,
+    val slektsforholdSpesifisering: Søknadsfelt<String?>,
     val idNummer: Søknadsfelt<String>,
     val adresse: Søknadsfelt<String>,
-    // Nye
-    val arbeidNorge: Søknadsfelt<String?>,
     val arbeidUtland: Søknadsfelt<String?>,
     val arbeidsperioderUtland: List<Søknadsfelt<Arbeidsperiode>> = listOf(),
+    val arbeidNorge: Søknadsfelt<String?>,
     val arbeidsperioderNorge: List<Søknadsfelt<Arbeidsperiode>> = listOf(),
     val pensjonUtland: Søknadsfelt<String?>,
     val pensjonsperioderUtland: List<Søknadsfelt<Pensjonsperiode>> = listOf(),
     val pensjonNorge: Søknadsfelt<String?>,
     val pensjonsperioderNorge: List<Søknadsfelt<Pensjonsperiode>> = listOf(),
+    val andreUtbetalinger: Søknadsfelt<String?>,
+    val andreUtbetalingsperioder: List<Søknadsfelt<Utbetalingsperiode>> = listOf(),
+    val pågåendeSøknadFraAnnetEøsLand: Søknadsfelt<String?>,
+    val pågåendeSøknadHvilketLand: Søknadsfelt<String?>,
+    val barnetrygdFraEøs: Søknadsfelt<String?>,
+    val eøsBarnetrygdsperioder: List<Søknadsfelt<EøsBarnetrygdsperiode>> = listOf(),
+)
+
+data class Søknaddokumentasjon(
+    val dokumentasjonsbehov: Dokumentasjonsbehov,
+    val harSendtInn: Boolean,
+    val opplastedeVedlegg: List<Søknadsvedlegg>,
+    val dokumentasjonSpråkTittel: Map<Locale, String>
+)
+
+enum class Dokumentasjonsbehov {
+    AVTALE_DELT_BOSTED,
+    VEDTAK_OPPHOLDSTILLATELSE,
+    ADOPSJON_DATO,
+    BEKREFTELSE_FRA_BARNEVERN,
+    BOR_FAST_MED_SØKER,
+    SEPARERT_SKILT_ENKE,
+    MEKLINGSATTEST,
+    ANNEN_DOKUMENTASJON,
+}
+
+data class Søknadsvedlegg(
+    val dokumentId: String,
+    val navn: String,
+    val tittel: Dokumentasjonsbehov,
+)
+
+data class AndreForelder(
+    val kanIkkeGiOpplysninger: Boolean,
+    val navn: Søknadsfelt<String>,
+    val fnr: Søknadsfelt<String>,
+    val fødselsdato: Søknadsfelt<String>,
+    val arbeidUtlandet: Søknadsfelt<String>,
+    val pensjonUtland: Søknadsfelt<String>,
+    val skriftligAvtaleOmDeltBosted: Søknadsfelt<String>,
+    val utvidet: AndreForelderUtvidet,
+    val pensjonNorge: Søknadsfelt<String?>,
+    val arbeidNorge: Søknadsfelt<String?>,
+    val andreUtbetalinger: Søknadsfelt<String?>,
+    val arbeidsperioderUtland: List<Søknadsfelt<Arbeidsperiode>> = listOf(),
+    val pensjonsperioderUtland: List<Søknadsfelt<Pensjonsperiode>> = listOf(),
+    val arbeidsperioderNorge: List<Søknadsfelt<Arbeidsperiode>> = listOf(),
+    val pensjonsperioderNorge: List<Søknadsfelt<Pensjonsperiode>> = listOf(),
+    val andreUtbetalingsperioder: List<Søknadsfelt<Utbetalingsperiode>> = listOf(),
+    val idNummer: List<Søknadsfelt<IdNummer>> = listOf(),
+    val adresse: Søknadsfelt<String>,
+    val pågåendeSøknadFraAnnetEøsLand: Søknadsfelt<String?>,
+    val pågåendeSøknadHvilketLand: Søknadsfelt<String?>,
+    val barnetrygdFraEøs: Søknadsfelt<String?>,
+    val eøsBarnetrygdsperioder: List<Søknadsfelt<EøsBarnetrygdsperiode>> = listOf(),
 )
