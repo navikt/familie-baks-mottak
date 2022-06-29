7be348aaadf0c1b903ddc4783538f9f434dd48fc
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-06-29 10:49:12 +0200

    Dirties context

--- src/test/kotlin/no/nav/familie/ba/mottak/task/FjernGamleHendelseLoggInnslagTest.kt
+++ src/test/kotlin/no/nav/familie/ba/mottak/task/FjernGamleHendelseLoggInnslagTest.kt
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -23,6 +24,7 @@ import java.time.LocalDateTime
 @ActiveProfiles("postgres")
 @Tag("integration")
 @SpringBootTest(classes = [DevLauncherPostgres::class])
+@DirtiesContext
 class FjernGamleHendelseLoggInnslagTest {
 
     @Autowired
