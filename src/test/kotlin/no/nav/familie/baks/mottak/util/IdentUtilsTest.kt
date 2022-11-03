875279a945bd18d7af6fd288645fccdee9815791
Stig Strøm <stig.helge.strom@nav.no>
2022-11-03 12:50:02 +0100

    erBost filtrer nå kun bost nummer (#757)

--- src/test/kotlin/no/nav/familie/baks/mottak/util/IdentUtilsTest.kt
+++ src/test/kotlin/no/nav/familie/baks/mottak/util/IdentUtilsTest.kt
@@ -7,19 +7,30 @@ import org.junit.jupiter.api.Test
 class IdentUtilsTest {
     @Test
     fun testErDnummer() {
-        Assertions.assertFalse(erDnummer(PersonIdent(id = "12345678910")))
-        Assertions.assertTrue(erDnummer(PersonIdent(id = "42345678910")))
+        Assertions.assertFalse(erDnummer(PersonIdent(id = FNR)))
+        Assertions.assertFalse(erDnummer(PersonIdent(id = NAV_SYNTETISK_ID)))
+        Assertions.assertTrue(erDnummer(PersonIdent(id = DNR)))
     }
 
     @Test
     fun `er BOST-nummer`() {
-        Assertions.assertFalse(erBostNummer(PersonIdent(id = "10102022222")))
-        Assertions.assertTrue(erBostNummer(PersonIdent(id = "10201022222")))
+        Assertions.assertFalse(erBostNummer(PersonIdent(id = FNR)))
+        Assertions.assertFalse(erBostNummer(PersonIdent(id = NAV_SYNTETISK_ID)))
+        Assertions.assertTrue(erBostNummer(PersonIdent(id = BOST_NR)))
     }
 
     @Test
     fun `er FDAT nummer`() {
-        Assertions.assertFalse(erFDatnummer(PersonIdent(id = "10102022222")))
-        Assertions.assertTrue(erFDatnummer(PersonIdent(id = "10201000000")))
+        Assertions.assertFalse(erFDatnummer(PersonIdent(id = FNR)))
+        Assertions.assertFalse(erFDatnummer(PersonIdent(id = NAV_SYNTETISK_ID)))
+        Assertions.assertTrue(erFDatnummer(PersonIdent(id = FDAT_NR)))
+    }
+
+    companion object {
+        const val BOST_NR = "10211022222"
+        const val NAV_SYNTETISK_ID = "31502200112"
+        const val FDAT_NR = "10201000000"
+        const val FNR = "10102022222"
+        const val DNR = "42345678910"
     }
 }
