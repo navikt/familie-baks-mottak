11c91c91e57dc150bc4355c2c4310b5c02dd58b0
Stig Helge Strøm <stig.helge.strom@nav.no>
2023-09-11 10:42:55 +0200

    Revert "Ikke send duplikate hendelser ved merge (#934)"
    This reverts commit 04a0a8c756a716f22f46a8eba7185fec8b3582b8.

--- src/main/kotlin/no/nav/familie/baks/mottak/hendelser/IdenthendelseV2Consumer.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/hendelser/IdenthendelseV2Consumer.kt
@@ -45,27 +45,18 @@ class IdenthendelseV2Consumer(
             SECURE_LOGGER.info("Har mottatt ident-hendelse $consumerRecord")
 
             val aktør = consumerRecord.value()
-            val aktørIdPåHendelse = consumerRecord.key()
 
             if (aktør == null) {
                 log.warn("Tom aktør fra identhendelse")
-                SECURE_LOGGER.warn("Tom aktør fra identhendelse med nøkkel $aktørIdPåHendelse")
+                SECURE_LOGGER.warn("Tom aktør fra identhendelse med nøkkel ${consumerRecord.key()}")
             }
 
-            val aktivAktørid = aktør?.identifikatorer?.singleOrNull { ident ->
-                ident.type == Type.AKTORID && ident.gjeldende
-            }?.idnummer.toString()
+            aktør?.identifikatorer?.singleOrNull { ident ->
+                ident.type == Type.FOLKEREGISTERIDENT && ident.gjeldende
+            }?.also { folkeregisterident ->
+                SECURE_LOGGER.info("Sender ident-hendelse til ba-sak for ident $folkeregisterident")
 
-            if (aktørIdPåHendelse == aktivAktørid) { // I tilfeller som ved merge av hendelser vil man få både identhendelse på gammel og ny aktørid, så for å unngå duplikater så sender man bare på aktiv ident
-                aktør?.identifikatorer?.singleOrNull { ident ->
-                    ident.type == Type.FOLKEREGISTERIDENT && ident.gjeldende
-                }?.also { folkeregisterident ->
-                    SECURE_LOGGER.info("Sender ident-hendelse til ba-sak for ident $folkeregisterident")
-
-                    sakClient.sendIdenthendelseTilSak(PersonIdent(ident = folkeregisterident.idnummer.toString()))
-                }
-            } else {
-                SECURE_LOGGER.info("Ignorerer å sende ident-hendelse til ba-sak for aktør $aktørIdPåHendelse ikke lenger gyldig aktør")
+                sakClient.sendIdenthendelseTilSak(PersonIdent(ident = folkeregisterident.idnummer.toString()))
             }
         } catch (e: RuntimeException) {
             identhendelseFeiletCounter.increment()
