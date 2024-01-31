0b4a158f79747855ea7133ecd7cf302712e95e35
Stig Helge Strøm <stig.helge.strom@nav.no>
2024-01-31 10:15:55 +0100

    Sletter kode som ikke skulle vært sjekket inn

--- src/main/kotlin/no/nav/familie/baks/mottak/task/JournalhendelseKontantstøtteRutingTask.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/task/JournalhendelseKontantstøtteRutingTask.kt
@@ -15,7 +15,6 @@ import no.nav.familie.prosessering.internal.TaskService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import java.time.YearMonth
 
 @Service
@@ -64,7 +63,6 @@ class JournalhendelseKontantstøtteRutingTask(
         sakssystemMarkeringCounter[saksystem]!!.increment()
     }
 
-    @Transactional
     private fun søkEtterSakIInfotrygd(brukersIdent: String): Boolean {
         val barnasIdenter =
             pdlClient.hentPersonMedRelasjoner(brukersIdent, Tema.KON).forelderBarnRelasjoner
