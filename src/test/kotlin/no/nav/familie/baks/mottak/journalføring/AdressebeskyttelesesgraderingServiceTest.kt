e398c5c6fcc215be1c0186312ffb0f977265fabb
thoalm <thoalm@outlook.com>
2024-09-12 14:02:22 +0200

    NAV-21852: La til manglende tester

--- src/test/kotlin/no/nav/familie/baks/mottak/journalføring/AdressebeskyttelesesgraderingServiceTest.kt
+++ src/test/kotlin/no/nav/familie/baks/mottak/journalføring/AdressebeskyttelesesgraderingServiceTest.kt
@@ -200,6 +200,121 @@ class AdressebeskyttelesesgraderingServiceTest {
         assertThat(finnesAdressebeskyttelsegradringPåJournalpost).isTrue()
     }
 
+    @ParameterizedTest
+    @EnumSource(
+        value = Tema::class,
+        names = ["BAR", "KON"],
+    )
+    fun `skal returnere false om journalpost fra digital søknad ikke er tilknyttet en strengt fotrolig person`(
+        tema: Tema,
+    ) {
+        // Arrange
+        val journalpost =
+            Journalpost(
+                journalpostId = "123",
+                journalposttype = Journalposttype.I,
+                journalstatus = Journalstatus.MOTTATT,
+                tema = tema.name,
+                journalforendeEnhet = "1",
+                kanal = "NAV_NO",
+                dokumenter = hentDokumenterMedRiktigBrevkode(tema),
+                bruker = Bruker("312", BrukerIdType.FNR),
+            )
+
+        every {
+            mockedSøknadsidenterService.hentIdenterForKontantstøtteViaJournalpost(
+                journalpostId = journalpost.journalpostId,
+            )
+        } returns Pair("123456789", emptyList())
+
+        every {
+            mockedSøknadsidenterService.hentIdenterForBarnetrygdViaJournalpost(
+                journalpostId = journalpost.journalpostId,
+            )
+        } returns Pair("123456789", emptyList())
+
+        every {
+            mockedPdlClient.hentPerson(
+                personIdent = "123456789",
+                graphqlfil = "hentperson-med-adressebeskyttelse",
+                tema = tema,
+            )
+        } returns
+            PdlPersonData(
+                adressebeskyttelse =
+                    listOf(
+                        Adressebeskyttelse(
+                            gradering = Adressebeskyttelsesgradering.UGRADERT,
+                        ),
+                    ),
+            )
+
+        // Act
+        val finnesAdressebeskyttelsegradringPåJournalpost =
+            adressebeskyttelesesgraderingService.finnesStrengtFortroligAdressebeskyttelsegraderingPåJournalpost(
+                tema = tema,
+                journalpost = journalpost,
+            )
+
+        // Assert
+        assertThat(finnesAdressebeskyttelsegradringPåJournalpost).isFalse()
+    }
+
+    @ParameterizedTest
+    @EnumSource(
+        value = Tema::class,
+        names = ["BAR", "KON"],
+    )
+    fun `skal returnere false om journalpost fra papirsøknad ikke er tilknyttet en strengt fotrolig person`(
+        tema: Tema,
+    ) {
+        // Arrange
+        val journalpost =
+            Journalpost(
+                journalpostId = "123",
+                journalposttype = Journalposttype.I,
+                journalstatus = Journalstatus.MOTTATT,
+                tema = tema.name,
+                journalforendeEnhet = "1",
+                kanal = "IKKE_NAV_NO",
+                dokumenter = hentDokumenterMedRiktigBrevkode(tema),
+                bruker = Bruker("312", BrukerIdType.FNR),
+            )
+
+        every {
+            mockedJournalpostBrukerService.tilPersonIdent(
+                bruker = journalpost.bruker!!,
+                tema = tema,
+            )
+        } returns "123456789"
+
+        every {
+            mockedPdlClient.hentPerson(
+                personIdent = "123456789",
+                graphqlfil = "hentperson-med-adressebeskyttelse",
+                tema = tema,
+            )
+        } returns
+            PdlPersonData(
+                adressebeskyttelse =
+                    listOf(
+                        Adressebeskyttelse(
+                            gradering = Adressebeskyttelsesgradering.UGRADERT,
+                        ),
+                    ),
+            )
+
+        // Act
+        val finnesAdressebeskyttelsegradringPåJournalpost =
+            adressebeskyttelesesgraderingService.finnesStrengtFortroligAdressebeskyttelsegraderingPåJournalpost(
+                tema = tema,
+                journalpost = journalpost,
+            )
+
+        // Assert
+        assertThat(finnesAdressebeskyttelsegradringPåJournalpost).isFalse()
+    }
+
     private fun hentDokumenterMedRiktigBrevkode(tema: Tema): List<DokumentInfo> {
         val brevkode =
             when (tema) {
