2f3bf4808717386e13fa594f1462dfd29249afd7
bragejahren <brage.ekroll.jahren@nav.no>
2024-09-12 11:34:32 +0200

    Skrevet de første testene av AdressebeskyttelsegraderingService

--- src/test/kotlin/no/nav/familie/baks/mottak/journalføring/AdressebeskyttelesesgraderingServiceTest.kt
+++ src/test/kotlin/no/nav/familie/baks/mottak/journalføring/AdressebeskyttelesesgraderingServiceTest.kt
@@ -1,9 +1,33 @@
 package no.nav.familie.baks.mottak.journalføring
 
-class AdressebeskyttelesesgraderingServiceTest {
-    /*
+import io.mockk.mockk
+import no.nav.familie.baks.mottak.integrasjoner.Bruker
+import no.nav.familie.baks.mottak.integrasjoner.BrukerIdType
+import no.nav.familie.baks.mottak.integrasjoner.DokumentInfo
+import no.nav.familie.baks.mottak.integrasjoner.Dokumentstatus
+import no.nav.familie.baks.mottak.integrasjoner.Journalpost
+import no.nav.familie.baks.mottak.integrasjoner.Journalposttype
+import no.nav.familie.baks.mottak.integrasjoner.Journalstatus
+import no.nav.familie.baks.mottak.integrasjoner.PdlClient
+import no.nav.familie.baks.mottak.integrasjoner.SøknadsidenterService
+import no.nav.familie.kontrakter.felles.Tema
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
-    TODO : Fix this test
+class AdressebeskyttelesesgraderingServiceTest {
+    private val mockedPdlClient: PdlClient = mockk()
+    private val mockedSøknadsidenterService: SøknadsidenterService = mockk()
+    private val mockedJournalpostBrukerService: JournalpostBrukerService = mockk()
+
+    private val adressebeskyttelesesgraderingService =
+        AdressebeskyttelesesgraderingService(
+            pdlClient = mockedPdlClient,
+            søknadsidenterService = mockedSøknadsidenterService,
+            journalpostBrukerService = mockedJournalpostBrukerService,
+        )
 
     @ParameterizedTest
     @EnumSource(
@@ -22,7 +46,7 @@ class AdressebeskyttelesesgraderingServiceTest {
                 journalstatus = Journalstatus.MOTTATT,
                 tema = tema.name,
                 journalforendeEnhet = "1",
-                kanal = digitalKanal,
+                kanal = "NAV_NO",
                 dokumenter = hentDokumenterMedRiktigBrevkode(tema),
                 bruker = Bruker("312", BrukerIdType.FNR),
             )
@@ -30,12 +54,73 @@ class AdressebeskyttelesesgraderingServiceTest {
         // Act & Assert
         val exception =
             assertThrows<IllegalStateException> {
-                enhetsnummerService.hentEnhetsnummer(journalpost)
+                adressebeskyttelesesgraderingService.finnesAdressebeskyttelsegradringPåJournalpost(tema, journalpost)
             }
 
         assertThat(exception.message).isEqualTo("Støtter ikke tema $tema")
     }
 
+    @Test
+    fun `skal kaste feil når bruker ikke er satt på journalpost`() {
+        // Arrange
+        val journalpost =
+            Journalpost(
+                journalpostId = "123",
+                journalposttype = Journalposttype.I,
+                journalstatus = Journalstatus.MOTTATT,
+                tema = Tema.BAR.name,
+                journalforendeEnhet = "1",
+                kanal = "NAV_NO",
+                dokumenter = hentDokumenterMedRiktigBrevkode(Tema.BAR),
+                bruker = null,
+            )
 
-     */
+        // Act & Assert
+        val exception =
+            assertThrows<IllegalStateException> {
+                adressebeskyttelesesgraderingService.finnesAdressebeskyttelsegradringPåJournalpost(Tema.BAR, journalpost)
+            }
+
+        assertThat(exception.message).isEqualTo("Bruker på journalpost ${journalpost.journalpostId} kan ikke være null")
+    }
+
+    @ParameterizedTest
+    @EnumSource(
+        value = Tema::class,
+        names = ["BAR", "KON"],
+    )
+    fun `asd`(
+        tema: Tema,
+    ) {
+        // Arrange
+        val journalpost =
+            Journalpost(
+                journalpostId = "123",
+                journalposttype = Journalposttype.I,
+                journalstatus = Journalstatus.MOTTATT,
+                tema = tema.name,
+                journalforendeEnhet = "1",
+                kanal = "NAV_NO",
+                dokumenter = hentDokumenterMedRiktigBrevkode(tema),
+                bruker = Bruker("312", BrukerIdType.FNR),
+            )
+
+        // Act & Assert
+        val exception =
+            assertThrows<IllegalStateException> {
+                adressebeskyttelesesgraderingService.finnesAdressebeskyttelsegradringPåJournalpost(tema, journalpost)
+            }
+
+        assertThat(exception.message).isEqualTo("Støtter ikke tema $tema")
+    }
+
+    private fun hentDokumenterMedRiktigBrevkode(tema: Tema): List<DokumentInfo> {
+        val brevkode =
+            when (tema) {
+                Tema.BAR -> "NAV 33-00.07"
+                Tema.KON -> "NAV 34-00.08"
+                else -> ""
+            }
+        return listOf(DokumentInfo(brevkode = brevkode, tittel = "Søknad", dokumentstatus = Dokumentstatus.FERDIGSTILT, dokumentvarianter = emptyList()))
+    }
 }
