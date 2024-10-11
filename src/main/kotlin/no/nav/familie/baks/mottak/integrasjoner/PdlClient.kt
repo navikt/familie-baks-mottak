9462cf460b3b461256d2d8ba8f44dab7547d423b
bragejahren <brage.ekroll.jahren@nav.no>
2024-10-11 13:10:59 +0200

    Fjerner utkommentert kode

--- src/main/kotlin/no/nav/familie/baks/mottak/integrasjoner/PdlClient.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/integrasjoner/PdlClient.kt
@@ -320,22 +320,6 @@ data class Sivilstand(
     val bekreftelsesdato: LocalDate? = null,
 )
 
-// enum class Adressebeskyttelsesgradering {
-//    STRENGT_FORTROLIG_UTLAND, // Kode 19
-//    FORTROLIG, // Kode 7
-//    STRENGT_FORTROLIG, // Kode 6
-//    UGRADERT,
-//    ;
-//
-//    fun erStrengtFortrolig() = this == STRENGT_FORTROLIG
-//
-//    fun erStrengtFortroligUtland() = this == STRENGT_FORTROLIG_UTLAND
-//
-//    fun erFortrolig(): Boolean = this == FORTROLIG
-//
-//    fun erUgradert(): Boolean = this == UGRADERT
-// }
-
 enum class Identgruppe {
     AKTORID,
     FOLKEREGISTERIDENT,
