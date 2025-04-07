8e8faa8b891758b8208df85dda1181f106575b7e
RobinLundSadun <robin.lund.sadun@gmail.com>
2025-04-07 15:43:10 +0200

    Fjerne lokal-kjøring test

--- src/test/kotlin/no/nav/familie/baks/mottak/søknad/FamiliePdfServiceTest.kt
+++ src/test/kotlin/no/nav/familie/baks/mottak/søknad/FamiliePdfServiceTest.kt
@@ -2,10 +2,7 @@ package no.nav.familie.baks.mottak.søknad
 
 import io.mockk.every
 import io.mockk.mockk
-import no.nav.familie.baks.mottak.søknad.barnetrygd.domene.DBBarnetrygdSøknad
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import java.io.File
 
 internal class FamiliePdfServiceTest {
     private val pdfClient: FamiliePdfClient = mockk()
@@ -17,20 +14,20 @@ internal class FamiliePdfServiceTest {
         every { pdfClient.opprettPdf(any()) } returns byteArrayOf(1, 2, 3)
     }
 
-    @Test
-    fun `Lag feltmaptest`() {
-        val barnetrygdSøknadSomString =
-            File("./src/test/resources/testdata/versjonertBarnetrygdSøknadV9.json")
-                .readText(Charsets.UTF_8)
-        val dbBarnetrygdSøknad =
-            DBBarnetrygdSøknad(
-                id = 2L,
-                søknadJson = barnetrygdSøknadSomString,
-                fnr = "1234123412",
-            )
-
-        val versjonert = dbBarnetrygdSøknad.hentVersjonertBarnetrygdSøknad()
-
-        val feltmap = familiePdfService.lagBarnetrygdPdfKvittering(dbBarnetrygdSøknad, "nb")
-    }
+//    @Test
+//    fun `Lag feltmaptest`() {
+//        val barnetrygdSøknadSomString =
+//            File("./src/test/resources/testdata/versjonertBarnetrygdSøknadV9.json")
+//                .readText(Charsets.UTF_8)
+//        val dbBarnetrygdSøknad =
+//            DBBarnetrygdSøknad(
+//                id = 2L,
+//                søknadJson = barnetrygdSøknadSomString,
+//                fnr = "1234123412",
+//            )
+//
+//        val versjonert = dbBarnetrygdSøknad.hentVersjonertBarnetrygdSøknad()
+//
+//        val feltmap = familiePdfService.lagBarnetrygdPdfKvittering(dbBarnetrygdSøknad, "nb")
+//    }
 }
