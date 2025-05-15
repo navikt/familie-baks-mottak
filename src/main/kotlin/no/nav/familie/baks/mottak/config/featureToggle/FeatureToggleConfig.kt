4f444560253a625a5119524647ac8df9dff91ec7
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-05-15 23:10:44 +0200

    Vi tillater automatisk journalføring av journalposter når enhet er 2103 eller det finnes strengt fortrolig personer når toggle er på

--- src/main/kotlin/no/nav/familie/baks/mottak/config/featureToggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/config/featureToggle/FeatureToggleConfig.kt
@@ -5,6 +5,9 @@ class FeatureToggleConfig {
         // Operasjonelle
         const val HOPP_OVER_INFOTRYGD_SJEKK = "familie-ba-sak.hopp-over-infotrygd-sjekk"
 
+        // Operasjonelle
+        const val AUTOMATISK_JOURNALFØR_ENHET_2103 = "familie-ba-sak.tillatt-behandling-av-kode6-kode19"
+
         // Ny pdf kvittering
         const val NY_FAMILIE_PDF_KVITTERING = "familie-ba-soknad.ny-pdf"
     }
