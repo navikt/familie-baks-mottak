44bca10943be8dc0d04e5956f08f1fc4f3fe2e84
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-08-06 10:26:34 +0200

    Hopp ut av task hvis bruker ikke har løpende barnetrygd

--- src/main/kotlin/no/nav/familie/baks/mottak/task/FinnmarkstilleggTask.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/task/FinnmarkstilleggTask.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.baks.mottak.task
 
+import no.nav.familie.baks.mottak.integrasjoner.BaSakClient
 import no.nav.familie.baks.mottak.integrasjoner.PdlClient
 import no.nav.familie.kontrakter.ba.finnmarkstillegg.kommuneErIFinnmarkEllerNordTroms
 import no.nav.familie.kontrakter.felles.Tema
@@ -19,12 +20,20 @@ import org.springframework.stereotype.Service
 )
 class FinnmarkstilleggTask(
     private val pdlClient: PdlClient,
+    private val baSakClient: BaSakClient,
 ) : AsyncTaskStep {
     override fun doTask(task: Task) {
         val ident = task.payload
+
+        val harIkkeLøpendeBarnetrygd = baSakClient.hentFagsakerHvorPersonMottarLøpendeUtvidetEllerOrdinærBarnetrygd(ident).isEmpty()
+        if (harIkkeLøpendeBarnetrygd) {
+            secureLogger.info("Fant ingen løpende barnetrygd for ident $ident, hopper ut av FinnmarkstilleggTask")
+            return
+        }
+
         val adresser = pdlClient.hentPerson(ident, "hentperson-med-bostedsadresse", Tema.BAR).bostedsadresse.filterNotNull()
         if (adresser.isEmpty()) {
-            secureLogger.info("Fant ingen bostedsadresser for ident $ident")
+            secureLogger.info("Fant ingen bostedsadresser for ident $ident, hopper ut av FinnmarkstilleggTask")
             return
         }
 
