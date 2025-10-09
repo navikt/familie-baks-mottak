53a2b6aef3eebb95a493580318760cb46cbcd873
Stig Helge Strøm <stig.helge.strom@nav.no>
2025-10-09 08:31:30 +0200

    Bytter til toggle ment for baks-mottak

--- src/main/kotlin/no/nav/familie/baks/mottak/config/featureToggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/config/featureToggle/FeatureToggleConfig.kt
@@ -3,7 +3,7 @@ package no.nav.familie.baks.mottak.config.featureToggle
 class FeatureToggleConfig {
     companion object {
         // Operasjonelle
-        const val HOPP_OVER_INFOTRYGD_SJEKK = "familie-ba-sak.hopp-over-infotrygd-sjekk"
+        const val HOPP_OVER_INFOTRYGD_SJEKK = "familie-baks-mottak.hopp-over-infotrygd-sjekk"
 
         // Operasjonelle
         const val AUTOMATISK_JOURNALFØR_ENHET_2103 = "familie-ba-sak.tillatt-behandling-av-kode6-kode19"
