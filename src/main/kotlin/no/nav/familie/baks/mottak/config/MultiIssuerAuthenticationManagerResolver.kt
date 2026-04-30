939f83600ecc3e671a76fbb007c25b1a54485259
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2026-04-30 10:42:41 +0200

    ktlint

--- src/main/kotlin/no/nav/familie/baks/mottak/config/MultiIssuerAuthenticationManagerResolver.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/config/MultiIssuerAuthenticationManagerResolver.kt
@@ -24,7 +24,6 @@ class MultiIssuerAuthenticationManagerResolver(
         val IDPORTEN_LOA_HIGH = "idporten-loa-high"
     }
 
-
     fun resolver(): JwtIssuerAuthenticationManagerResolver =
         JwtIssuerAuthenticationManagerResolver { issuer ->
             when (issuer.trimEnd('/')) {
