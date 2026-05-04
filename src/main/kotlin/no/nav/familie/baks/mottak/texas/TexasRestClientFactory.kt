ea5124895bd36b140b5cfc22b49f2cde4a6aaf33
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-05-04 15:21:45 +0200

    Fjern unødvendig appName-parameter fra TexasRestClientFactory og legg til dokumentasjon for REST-klientalternativer

--- src/main/kotlin/no/nav/familie/baks/mottak/texas/TexasRestClientFactory.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/texas/TexasRestClientFactory.kt
@@ -2,7 +2,6 @@ package no.nav.familie.baks.mottak.texas
 
 import no.nav.familie.restklient.interceptor.ConsumerIdClientInterceptor
 import no.nav.familie.restklient.interceptor.MdcValuesPropagatingClientInterceptor
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Import
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
@@ -16,7 +15,6 @@ class TexasRestClientFactory(
     private val texasClient: TexasClient,
     private val consumerIdClientInterceptor: ConsumerIdClientInterceptor,
     private val mdcValuesPropagatingClientInterceptor: MdcValuesPropagatingClientInterceptor,
-    @Value("\${NAIS_APP_NAME}") private val appName: String,
 ) {
     fun lagMaskinRestKlient(target: String): RestClient =
         RestClient
