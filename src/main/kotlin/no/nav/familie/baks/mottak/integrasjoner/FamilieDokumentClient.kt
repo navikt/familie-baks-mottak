d697c3fc7e888a93f5254d7aad75156ff1bac31d
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-05-13 12:29:17 +0200

    Flyttet ut restKlient for familie-dokument ut til config

--- src/main/kotlin/no/nav/familie/baks/mottak/integrasjoner/FamilieDokumentClient.kt
+++ src/main/kotlin/no/nav/familie/baks/mottak/integrasjoner/FamilieDokumentClient.kt
@@ -1,13 +1,10 @@
 package no.nav.familie.baks.mottak.integrasjoner
 
-import no.nav.familie.felles.tokenklient.tokenx.TokenXClient
 import no.nav.familie.kontrakter.felles.Ressurs
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
@@ -18,20 +15,11 @@ private val logger = LoggerFactory.getLogger(FamilieDokumentClient::class.java)
 @Component
 class FamilieDokumentClient(
     @param:Value("\${FAMILIE_DOKUMENT_API_URL}") private val dokumentUri: URI,
-    @param:Value("\${FAMILIE_DOKUMENT_SCOPE}") private val familieDokumentScope: String,
-    private val tokenxClient: TokenXClient,
     @Qualifier("unauthenticatedRestClient") private val unauthenticatedRestClient: RestClient,
+    @Qualifier("familieTokenXRestClient") private val restClient: RestClient,
 ) {
     fun hentVedlegg(dokumentId: String): ByteArray {
         logger.info("Henter vedlegg med dokumentid $dokumentId")
-
-        val nåværendeContext = SecurityContextHolder.getContext().authentication as JwtAuthenticationToken
-
-        val nyttToken =
-            tokenxClient
-                .hentToken(familieDokumentScope, nåværendeContext.token.tokenValue)
-
-        val restClient = RestClient.builder().defaultHeader("Authorization", "Bearer $nyttToken").build()
 
         val uri = URI.create("$dokumentUri/api/mapper/ANYTHING/$dokumentId")
 
